68a998bcf7fa73ac5a6f9bc457d7290241d92fa7
RyannKim327 <rksesgundo123@gmail.com>
2023-10-26 14:55:06 +0800

    10/26/2023 2:55:06 PM [Autogit]

--- script/info.js
+++ script/info.js
@@ -55,7 +55,10 @@ module.exports = async (api, event) => {
 	let json = JSON.parse(fs.readFileSync("data/preferences.json", "utf8"))
 	let myname = () => {
 		if(user[myID]['name'].toLowerCase() === getName(myID).toLowerCase()){
-			if(user[myID]['name'].toLowerCase().includes("bot"))
+			let usn = 
+			if(user[myID]['name'].toLowerCase().includes("bot")){
+
+			}
 			return user[myID]['name']
 		}else{
 			return user[myID]['name'] + " you may also call me " + getName()
