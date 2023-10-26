178390abcdf90fb05b2762f5dc1b9299d3e66f15
RyannKim327 <rksesgundo123@gmail.com>
2023-10-27 03:50:33 +0800

    10/27/2023 3:50:33 AM [Autogit]

--- game/hanggame.js
+++ game/hanggame.js
@@ -1,9 +1,14 @@
 const axios = require("axios")
 const fs = require("fs")
+const regex = require("../utils/regex")
 
 async function getWord(){
 	const { data } = await axios.get("https://random-word-api.herokuapp.com/word")
 	return data
 }
 
-MessagePort
+module.exports = async (api, event, regex) => {
+	const word = await getWord()
+	const data = event.body.match(regex)
+	
+}
