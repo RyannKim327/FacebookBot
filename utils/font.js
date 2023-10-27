720a9e0bd710f53769e93fef9c96ba1376656871
RyannKim327 <rksesgundo123@gmail.com>
2023-10-27 19:15:05 +0800

    10/27/2023 7:15:04 PM [Autogit]

--- utils/font.js
+++ utils/font.js
@@ -1,4 +1,6 @@
 module.exports = (word) => {
 	const frank = "𝔄𝔅ℭ𝔇𝔈𝔉𝔊ℌℑ𝔍𝔎𝔏𝔐𝔑𝔒𝔓𝔔ℜ𝔖𝔗𝔘𝔙𝔚𝔛𝔜ℨ𝔞𝔟𝔠𝔡𝔢𝔣𝔤𝔥𝔦𝔧𝔨𝔩𝔪𝔬𝔭𝔮𝔯𝔰𝔱𝔲𝔳𝔴𝔵𝔶𝔷"
-	const alpha = "	"
+	const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmopqrstuvwxyz"
+	let res = ""
+	for 
 }
