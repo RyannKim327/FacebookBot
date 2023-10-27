c75d78fbe3bdce2854ef33ccab1e5931eebc7f6e
RyannKim327 <rksesgundo123@gmail.com>
2023-10-28 06:23:33 +0800

    10/28/2023 6:23:33 AM [Autogit]

--- cron/music.js
+++ cron/music.js
@@ -54,9 +54,9 @@ module.exports = async (api, event) => {
 	let json = JSON.parse(fs.readFileSync("data/songs.json", "utf8"))
 	let json2 = JSON.parse(fs.readFileSync("data/preferences.json", "utf8"))
 	let songs = json.links
-	let song = songs[Math.random() * ]
+	let song = songs[Math.random() * songs.length]
 	if(!fs.existsSync(name)){
-		let data = dl()
+		let data = await dl(song)
 		let file = fs.createWriteStream(`temp/${event}_worship.mp3`)
 		http.get()
 	}
