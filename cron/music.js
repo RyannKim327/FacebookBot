45e1a0f28dbfdebd4ee4cd8f79a236b23f598766
RyannKim327 <rksesgundo123@gmail.com>
2023-10-29 07:26:54 +0800

    10/29/2023 7:26:54 AM [Autogit]

--- cron/music.js
+++ cron/music.js
@@ -17,21 +17,60 @@ module.exports = async (api, event) => {
 	let songs = json.links
 	let song = songs[Math.floor(Math.random()) * songs.length]
 	if(!fs.existsSync(name)){
-		dl(song).then(response => {
-			let file = fs.createWriteStream(`temp/${event}_worship.mp3`)
-			http.get(response[0], (r) => {
-				r.pipe(file)
-				file.on("finish", () => {
-					api.sendMessage({
-						body: `A blessed sunday everyone, a song entitled ${response[1]} was sent to this thread.`,
-						attachment: fs.createReadStream(name).on("end", () => {
-							if(fs.existsSync(name)){
-								fs.unlink(name, (e) => {})
-							}
-						})
-					}, event, (e, m) => {})
-				})
+		try{
+			const json = JSON.parse(fs.readFileSync("data/preferences.json"))
+			const file = fs.createWriteStream(`temp/${event.threadID}_${event.senderID}.mp3`)
+			api.setMessageReaction("🔎", event.messageID, (e) => {}, true)
+			const data = event.body.match(regex)[1]
+			await yt.initalize()
+			const music = await yt.search(data.replace(/[^\w\s]/gi, ''))
+			if(music.content.length <= 0){
+				throw new Error(`${data.replace(/[^\w\s]/gi, '')} returned no results found`)
+			}else{
+				if(music.content[0].videoId == undefined){
+					throw new Error(`${data.replace(/[^\w\s]/gi, '')} is not found on youtube music. Try to add the singer, maybe I can find it.`)
+				}
+			}
+			const url = `https://www.youtube.com/watch?v=${music.content[0].videoId}`
+			const strm = ytdl(url, {
+				quality: "lowest"
 			})
-		})
+			const info = await ytdl.getInfo(url)
+			api.setMessageReaction("⏳", event.messageID, (e) => {}, true)
+			let user = await api.getUserInfo(event.senderID)
+			let g = gender(user[event.senderID]['firstName'])['eng']
+			let reqBy = `${g} ${user[event.senderID]['name']}`
+			ffmpegs(strm).audioBitrate(96).save(`${__dirname}/../temp/${event.threadID}_${event.senderID}.mp3`).on("end", async () => {
+				api.sendMessage({
+					body: `Here's your requests ${reqBy}:\nTitle: ${font(info.videoDetails.title)}\nUploaded by: ${info.videoDetails.author.name}`,
+					mentions:[{
+						id: event.senderID,
+						tag: user[event.senderID]['name']
+					}],
+					attachment: fs.createReadStream(`${__dirname}/../temp/${event.threadID}_${event.senderID}.mp3`).on("end", async () => {
+						if(fs.existsSync(`${__dirname}/../temp/${event.threadID}_${event.senderID}.mp3`)){
+							fs.unlink(`${__dirname}/../temp/${event.threadID}_${event.senderID}.mp3`, (err) => {
+								if(err){
+									console.log(err)
+								}
+								api.setMessageReaction("", event.messageID, (e) => {}, true)
+								console.log("Done")
+							})
+						}
+					})
+				}, event.threadID, (e, m) => {
+					if(e){
+						api.sendMessage(e.message, event.threadID, (e, m) => {
+							afk(api, json)
+						})
+					}
+				})
+				api.setMessageReaction("", event.messageID, (e) => {}, true)
+			})
+		}catch(err){
+			console.log(err)
+			api.sendMessage("Error: " + err, event.threadID, event.messageID)
+			api.setMessageReaction("", event.messageID, (e) => {}, true)
+		}
 	}
 }
