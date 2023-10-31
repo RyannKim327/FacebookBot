0d94ebe4e86732212af6fb3483a6a7de0e19ce0b
RyannKim327 <rksesgundo123@gmail.com>
2023-10-31 19:10:40 +0800

    10/31/2023 7:10:40 PM [Autogit]

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-const font = require("./utils/font")
-
-console.log(font("Hello world"))
+const yt_1 = /youtube.com\/watch\?v=([a-zA-Z0-9-_]{11}$)/
+		const yt_2 = /youtu.be\/([a-zA-Z0-9-_]{11}$)/
+		
