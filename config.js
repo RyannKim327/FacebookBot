2c9deae47a398faf689fb159af365aec4be8bb78
RyannKim327 <rksesgundo123@gmail.com>
2023-10-31 14:32:27 +0800

    10/31/2023 2:32:27 PM [Autogit]

--- config.js
+++ config.js
@@ -411,6 +411,7 @@ let start = (state) => {
 		let getData = Math.floor(Math.random() * 100)
 		if(options.selfListen)
 			admins.push(self)
+		
 		// if(autoBot && (getData % 10) == 0){
 		// 	admins.forEach(id => {
 		// 		if(bot.includes(id) && bot == self)
