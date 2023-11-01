1ceacfb042dea1e410131a93201e8c58dbfb5f43
RyannKim327 <rksesgundo123@gmail.com>
2023-11-01 08:44:53 +0800

    11/1/2023 8:44:53 AM [Autogit]

--- test.js
+++ test.js
@@ -1,10 +1,4 @@
 const yt_1 = /youtube.com\/watch\?v=([a-zA-Z0-9-_]{11}$)/
 const yt_2 = /youtu.be\/([a-zA-Z0-9-_]{11}$)/
 
-const a = "Hi"
-
-if(a){
-	console.log("Test")
-}else{
-	console.log("Hey")
-}
+console.log("Test sync")
