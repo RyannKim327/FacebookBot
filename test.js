77b5f6df472e265944e6d9943723deb3151be707
RyannKim327 <rksesgundo123@gmail.com>
2023-11-01 09:00:54 +0800

    11-01-23 09:00

--- test.js
+++ test.js
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
 const yt_1 = /youtube.com\/watch\?v=([a-zA-Z0-9-_]{11}$)/
 const yt_2 = /youtu.be\/([a-zA-Z0-9-_]{11}$)/
 
-console.log("Test sync")
-=======
-const yt_1 = /youtube.com\/watch\?v=([a-zA-Z0-9-_]{11}$)/
-const yt_2 = /youtu.be\/([a-zA-Z0-9-_]{11}$)/
-
-const a = "Hi"
-
-if(a){
-	console.log("Test")
-}else{
-	console.log("Hey")
-}
->>>>>>> 0ba36494eb475041b5cc06ce8de388cc2e86c691
+console.log("test")
