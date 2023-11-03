dedb23fd1d7fac373940361912b0073beacb62b2
RyannKim327 <rksesgundo123@gmail.com>
2023-11-03 15:43:55 +0800

    11/3/2023 3:43:55 PM

--- admin/vips.js
+++ admin/vips.js
@@ -12,8 +12,8 @@ module.exports = (api, event) => {
 			vips += `${event.messageReply.senderID}, `
 			api.sendMessage("User is a vip for about an hour", event.threadID, (e, m) => {})
 			setTimeout(() => {
+				vips.replace( `${event.messageReply.senderID}, `, "")
 				api.sendMessage("Expired na yung 1 hr free trial.", event.threadID, (e, m) => {}, event.messageReply.messageID)
-				vips.replace(event.messageReply.senderID, "")
 			}, ((1000 * 60) * 60))
 		}
 	}
