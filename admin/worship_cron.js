c8024c6477f3f96f036da7552040fbe12283fa70
RyannKim327 <rksesgundo123@gmail.com>
2023-11-03 11:11:14 +0800

    11/3/2023 11:11:13 AM

--- admin/worship_cron.js
+++ admin/worship_cron.js
@@ -2,5 +2,5 @@ const worship = require("./../cron/music")
 
 module.exports = async (api, event) => {
 	api.setMessageReaction("⏳", event.messageID, (e) => {}, true)
-	worship(api, event.theadID)
+	worship(api, event.threadID)
 }
