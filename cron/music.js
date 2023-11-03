90b03e295f08b034d73f7df6d19a43aa853ee11e
RyannKim327 <rksesgundo123@gmail.com>
2023-11-03 11:19:37 +0800

    11/3/2023 11:19:37 AM

--- cron/music.js
+++ cron/music.js
@@ -48,7 +48,7 @@ module.exports = async (api, event) => {
 				}else{
 					let user = await api.getUserInfo(event)
 					console.log(user[event])
-					let name = `${gender(user[event]['firstName']['eng'])} ${user[event]['name']}`
+					let name = `${gender(user[event]['name']['eng'])} ${user[event]['name']}`
 					api.sendMessage({
 						body: `Here's a random worship song sent to you ${name}:\nTitle: ${font(info.videoDetails.title)}\nUploaded by: ${info.videoDetails.author.name}`,
 						attachment: fs.createReadStream(`${__dirname}/../temp/${event}_worship.mp3`).on("end", async () => {
