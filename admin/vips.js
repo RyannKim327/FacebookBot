f615ca51d015b1cf29537b2a03de4c9d1b3786d2
RyannKim327 <rksesgundo123@gmail.com>
2023-11-03 15:16:27 +0800

    11/3/2023 3:16:27 PM

--- admin/vips.js
+++ admin/vips.js
@@ -1,7 +1,12 @@
 const { vips } = require("./../config")
+
+let checker = (id) => {
+	return vips.includes(id)
+}
+
 module.exports = (api, event) => {
-	if()
 	if(event.type == "message_reply"){
+		if(checker(event.messageReply.senderID))
 		vips += event.messageReply.senderID
 	}
 }
