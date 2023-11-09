8d0170ddf1ae2cb12000830bf927fe0eae7b2a13
RyannKim327 <rksesgundo123@gmail.com>
2023-11-09 17:29:29 +0800

    11/9/2023 5:29:29 PM

--- auto/openai.js
+++ auto/openai.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
 
 module.exports = async (api, event) => {
-	let { data } = await axios.get(``)
+	let { data } = await axios.get(`https://gpt4-ni-kim.hiroshiapi.repl.co`)
 }
