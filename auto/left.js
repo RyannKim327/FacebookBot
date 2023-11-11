c7839280daecde7b6dadc960a02895dca92dcc3b
RyannKim327 <rksesgundo123@gmail.com>
2023-11-11 22:11:47 +0800

    11/11/2023 10:11:46 PM

--- auto/left.js
+++ auto/left.js
@@ -20,7 +20,8 @@ module.exports = async (api, event) => {
 			const yt = ytdl(`https://www.youtube.com/watch?v=${song}`, {
 				quality: "lowestaudio"
 			})
-			const info = yt
+			const info = ytdl.getInfo(`https://www.youtube.com/watch?v=${song}`)
+			ytdl.get
 		}
 	}
 }
