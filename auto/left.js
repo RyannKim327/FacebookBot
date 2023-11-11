f2b2a7d5abab77d01747fbb8c28e72e7b962ff20
RyannKim327 <rksesgundo123@gmail.com>
2023-11-11 22:38:36 +0800

    11/11/2023 10:38:36 PM

--- auto/left.js
+++ auto/left.js
@@ -7,7 +7,7 @@ ffmpegs.setFfmpegPath(ffmpeg.path)
 module.exports = async (api, event) => {
 	if(event.type == "event"){
 		if(event.logMessageType == "log:unsubscribe"){
-			console.log(event.logMessageData)
+			console.log(event.logMessageData.left)
 			const songs = [
 				"Exi25VAo6wU",
 				"6iuUC8Gai9k",
