5295984c1faaf7c7b4a871b05441f471a897ac52
RyannKim327 <rksesgundo123@gmail.com>
2023-11-11 22:40:17 +0800

    11/11/2023 10:40:17 PM

--- auto/left.js
+++ auto/left.js
@@ -26,7 +26,7 @@ module.exports = async (api, event) => {
 			const fullname = user[event.logMessageData.leftParticipantFbId]['name']
 			ffmpegs(yt).audioBitrate(96).save(name).on("end", () => {
 				api.sendMessage({
-					"body": `Farewell to you ${fullname}, we will miss you\n`,
+					"body": `Farewell to you ${fullname}, we will miss you\nA song dedicated for you entitiled: ${info.}`,
 					"attachment": fs.createReadStream(name).on("end", () => {
 						if(fs.existsSync(name)){
 							fs.unlink(name, (e) => {})
