1bf0c7a3d8284dfef65ac9ea180b4f7a3d11b679
RyannKim327 <rksesgundo123@gmail.com>
2023-11-11 22:05:04 +0800

    11/11/2023 10:05:04 PM

--- auto/left.js
+++ auto/left.js
@@ -4,3 +4,6 @@ const ffmpeg = require('@ffmpeg-installer/ffmpeg')
 const ffmpegs = require('fluent-ffmpeg')
 ffmpegs.setFfmpegPath(ffmpeg.path)
 
+module.exports = async (api, event) => {
+	if(event.)
+}
