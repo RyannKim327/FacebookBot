3d4326c4a3627bfbb73a82827dd160741ced56f8
RyannKim327 <rksesgundo123@gmail.com>
2023-11-11 22:04:44 +0800

    11/11/2023 10:04:43 PM

--- auto/left.js
+++ auto/left.js
@@ -1,1 +1,6 @@
-const youtube = 
+const fs = require("fs")
+const ytdl = require('ytdl-core');
+const ffmpeg = require('@ffmpeg-installer/ffmpeg')
+const ffmpegs = require('fluent-ffmpeg')
+ffmpegs.setFfmpegPath(ffmpeg.path)
+
