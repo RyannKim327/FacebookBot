358dcfd86e93a87aa16e8642e783fdde7261f7d0
RyannKim327 <rksesgundo123@gmail.com>
2023-11-11 22:21:30 +0800

    11/11/2023 10:21:30 PM

--- auto/left.js
+++ auto/left.js
@@ -21,7 +21,7 @@ module.exports = async (api, event) => {
 				quality: "lowestaudio"
 			})
 			const info = await ytdl.getInfo(`https://www.youtube.com/watch?v=${song}`)
-			const name = `${__dirname}/../temp/${event.senderID}_farewell.mp3`
+			const name = `${__dirname}/../temp/${event.author}_farewell.mp3`
 			ffmpegs(yt).audioBitrate(96).save(name).on("end", () => {
 				api.sendMessage({
 					"body": "Babye test",
