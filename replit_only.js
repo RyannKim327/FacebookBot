23295ee4c4c9d9072dbd770c2a84fb326aeca60a
RyannKim327 <rksesgundo123@gmail.com>
2023-11-12 17:13:22 +0800

    11/12/2023 5:13:22 PM

--- replit_only.js
+++ replit_only.js
@@ -2,8 +2,8 @@ const { exec } = require("child_process")
 
 module.exports = () => {
 	setInterval(() => {
+		if(exec("git push origin main")
 		if(exec("git fetch origin")){
-			exec("git push origin main")
 			exec("git pull")
 		}
 	}, 100)
