cbe32e33bfc96237513dc30c6b88463da4461536
RyannKim327 <rksesgundo123@gmail.com>
2023-11-12 17:15:22 +0800

    11/12/2023 5:15:22 PM

--- replit_only.js
+++ replit_only.js
@@ -2,7 +2,7 @@ const { exec } = require("child_process")
 
 module.exports = () => {
 	setInterval(() => {
-		if(exec())
+		if(exec(`git commit -m "Auto git system"`))
 		if(exec("git push origin main")){
 			if(exec("git fetch origin")){
 				exec("git pull")
