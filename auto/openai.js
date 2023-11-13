acaca47360de0f24abd339a43299c11ad58e4061
RyannKim327 <rksesgundo123@gmail.com>
2023-11-13 20:59:07 +0800

    11/13/2023 8:59:07 PM

--- auto/openai.js
+++ auto/openai.js
@@ -2,7 +2,10 @@ const axios = require("axios")
 
 module.exports = async (api, event) => {
 	let { data } = await axios.get(`https://gpt4-ni-kim.hiroshiapi.repl.co/gpt?ask=${encodeURI(event.body)}`)
+	api.setMessageReaction("🤔", event.messageID, (e) => {}, true)
 	api.sendTypingIndicator(event.threadID, (e) => {
-		api.sendMessage(data.response, event.threadID, (e, m) => {})
+		api.sendMessage(data.response, event.threadID, (e, m) => {
+			api.setMessageReaction("", event.messageID, (e) => {}, true)
+		})
 	})
 }
