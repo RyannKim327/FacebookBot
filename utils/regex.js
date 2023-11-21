bbb26fe6c553a9c2cc8a9544e552a38f44d95763
RyannKim327 <rksesgundo123@gmail.com>
2023-11-22 00:01:45 +0800

    11/22/2023 12:01:45 AM

--- utils/regex.js
+++ utils/regex.js
@@ -1,13 +1,7 @@
 module.exports = (str) => {
-	let s = str
-	if(s.startsWith("/")){
-		s = s.replace("/", "\\/")
-	}
-	if(s.startsWith("?")){
-		s = s.replace("?", /\\?/)
-	}
+	let s = str.replace("/", "\\/").replace("?", /\\?/)
 	if(s.startsWith("$")){
-		s = s.replace("$", /\$/)
+		s = s.replace("$", )
 	}
 	return new RegExp("^" + s, "i")
 }
