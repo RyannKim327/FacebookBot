f33969e3c1bf24f4c1efb9c9173988ea3014fa23
RyannKim327 <rksesgundo123@gmail.com>
2023-11-24 23:01:19 +0800

    11/24/2023 11:01:19 PM

--- test.js
+++ test.js
@@ -1,21 +1,24 @@
-const fs = require("fs")
-const yt_1 = /youtube.com\/watch\?v=([a-zA-Z0-9-_]{11}$)/
-const yt_2 = /youtu.be\/([a-zA-Z0-9-_]{11}$)/
+// const fs = require("fs")
+// const yt_1 = /youtube.com\/watch\?v=([a-zA-Z0-9-_]{11}$)/
+// const yt_2 = /youtu.be\/([a-zA-Z0-9-_]{11}$)/
 
-const c = [
-	{
-		c: [
-			"hello", "world"
-		]
-	},{
-		c: [
-			"hi", "world"
-		]
-	}
-]
+// const c = [
+// 	{
+// 		c: [
+// 			"hello", "world"
+// 		]
+// 	},{
+// 		c: [
+// 			"hi", "world"
+// 		]
+// 	}
+// ]
 
-const a = JSON.parse(fs.readFileSync("x.json", "utf-8"))
+// const a = JSON.parse(fs.readFileSync("x.json", "utf-8"))
 
-for(let c in a){
-	console.log(a[c])
-}
+// for(let c in a){
+// 	console.log(a[c])
+// }
+
+let a = "hello world"
+a.shit
