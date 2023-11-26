824910efb2984c4920e0a9653ca88c7e920acf75
RyannKim327 <rksesgundo123@gmail.com>
2023-11-27 00:05:10 +0800

    11/27/2023 12:05:10 AM

--- test.js
+++ test.js
@@ -1,30 +1,3 @@
-// const fs = require("fs")
-// const yt_1 = /youtube.com\/watch\?v=([a-zA-Z0-9-_]{11}$)/
-// const yt_2 = /youtu.be\/([a-zA-Z0-9-_]{11}$)/
-
-// const c = [
-// 	{
-// 		c: [
-// 			"hello", "world"
-// 		]
-// 	},{
-// 		c: [
-// 			"hi", "world"
-// 		]
-// 	}
-// ]
-
-// const a = JSON.parse(fs.readFileSync("x.json", "utf-8"))
-
-// for(let c in a){
-// 	console.log(a[c])
-// }
-
-// let a = "hello world po".split(" ")
-// a.shift()
-// console.log(a.join(" "))
-
 const fs = require("fs")
-const data = fs.readFileSync("a.txt", "utf-8")
-const default_key = "G"
-const to_key = "A"
+
+let changeKey = (default)
