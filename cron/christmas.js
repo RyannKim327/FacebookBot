24cdb85921a443994a18b862d0d40bac076784a3
RyannKim327 <rksesgundo123@gmail.com>
2023-11-27 00:29:00 +0800

    11/27/2023 12:29:00 AM

--- cron/christmas.js
+++ cron/christmas.js
@@ -51,7 +51,7 @@ module.exports = async (api, event) => {
 					const g =	 gender(user[event]['firstName'])['eng']
 					let name = `${g} ${user[event]['name']}`
 					api.sendMessage({
-						body: `Here's a random worship song sent to you ${name}:\nTitle: ${font(info.videoDetails.title)}\nUploaded by: ${info.videoDetails.author.name}`,
+						body: `Happy New Year to you ${name}:\nTitle: ${font(info.videoDetails.title)}\nUploaded by: ${info.videoDetails.author.name}\n\nThank you for being a part of my 2023.`,
 						attachment: fs.createReadStream(`${__dirname}/../temp/${event}_worship.mp3`).on("end", async () => {
 							if(fs.existsSync(`${__dirname}/../temp/${event}_worship.mp3`)){
 								fs.unlink(`${__dirname}/../temp/${event}_worship.mp3`, (err) => {
