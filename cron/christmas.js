e9fdeeb2c10559e6f078294457cd007e7d707f14
RyannKim327 <rksesgundo123@gmail.com>
2023-11-27 00:28:00 +0800

    11/27/2023 12:27:59 AM

--- cron/christmas.js
+++ cron/christmas.js
@@ -30,7 +30,7 @@ module.exports = async (api, event) => {
 			ffmpegs(strm).audioBitrate(96).save(`${__dirname}/../temp/${event}_worship.mp3`).on("end", async () => {
 				if(thread.isGroup){
 					api.sendMessage({
-						body: `Happy new year ${thread.threadName}, here's a simple greetings for all of you:\nTitle: ${font(info.videoDetails.title)}\nUploaded by: ${info.videoDetails.author.name}`,
+						body: `Happy new year ${thread.threadName}, here's a simple greetings for all of you:\nTitle: ${font(info.videoDetails.title)}\nUploaded by: ${info.videoDetails.author.name}\n\nThank you for being part of my 2023`,
 						attachment: fs.createReadStream(`${__dirname}/../temp/${event}_worship.mp3`).on("end", async () => {
 							if(fs.existsSync(`${__dirname}/../temp/${event}_worship.mp3`)){
 								fs.unlink(`${__dirname}/../temp/${event}_worship.mp3`, (err) => {
