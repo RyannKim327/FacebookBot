0dc5958c2ccc404f1afbe8ba2f2e045fb4d3c71a
RyannKim327 <rksesgundo123@gmail.com>
2023-11-27 01:17:42 +0800

    11/27/2023 1:17:42 AM

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const YoutubeMusicApi = require("youtube-music-api");
 let a = async () => {
 	let b = new YoutubeMusicApi()
 	await b.initalize()
-	let c = await b.getPlaylist("PLyijK8r_zE5J1a5mrLxgxraLFRnNN5HDL")['content']
+	let c = await b.getPlaylist("PLyijK8r_zE5J1a5mrLxgxraLFRnNN5HDL")
 	let _music = c.content[Math.floor(Math.random() * c.content.length)]
 	console.log(_music)
 }
