fcf826fde2cfb6d5bdfa8c25ec01ad408a07e947
RyannKim327 <rksesgundo123@gmail.com>
2023-11-27 00:18:15 +0800

    11/27/2023 12:18:15 AM

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 const fs = require("fs")
 
+let replacement = ()
+
 let changeKey = (chords, default_key, change_key) => {
 	const keys = [
 		"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B",
