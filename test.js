7c82b7e259e27e25a92ea54741cff2d4b2379f1a
RyannKim327 <rksesgundo123@gmail.com>
2023-11-27 00:55:13 +0800

    11/27/2023 12:55:13 AM

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const YoutubeMusicApi = require("youtube-music-api");
 let a = async () => {
 	let b = new YoutubeMusicApi()
 	await b.initalize()
-	let c = await b.search("sana")
+	let c = await b.getPlaylist("PLyijK8r_zE5J1a5mrLxgxraLFRnNN5HDL")
 	console.log(c)
 }
 a()
