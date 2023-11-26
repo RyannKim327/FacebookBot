891ab9a7d7a07fe8328dcf17f9451575d51849b5
RyannKim327 <rksesgundo123@gmail.com>
2023-11-27 00:48:28 +0800

    11/27/2023 12:48:28 AM

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const YoutubeMusicApi = require("youtube-music-api");
 
 let a = async () => {
 	let b = new YoutubeMusicApi()
-	let c = b.getArtist("@Reyermusic")
-	console.log(c)
+	let c = b.getPlaylist("PLyijK8r_zE5J1a5mrLxgxraLFRnNN5HDL")
+	console.log(c.data)
 }
 a()
