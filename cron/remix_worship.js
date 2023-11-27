70d1a7a18448bc1a4b59a4f54814600f343571f6
RyannKim327 <rksesgundo123@gmail.com>
2023-11-27 08:47:13 +0800

    11/27/2023 8:47:13 AM

--- cron/remix_worship.js
+++ cron/remix_worship.js
@@ -24,11 +24,10 @@ module.exports = async (api, event) => {
 			quality: "lowestaudio"
 		})
 		const info = await ytdl.getInfo(url)
-		console.log(thread.isGroup)
 		ffmpegs(strm).audioBitrate(96).save(`${__dirname}/../temp/${event}_worship_remix.mp3`).on("end", async () => {
-			if(event.isGroup){
+			if(thread.isGroup){
 				api.sendMessage({
-					body: `Happy new year ${event.name}, here's a simple greetings for all of you:\nTitle: ${font(info.videoDetails.title)}\nUploaded by: ${info.videoDetails.author.name}\n\nThank you for being part of my 2023`,
+					body: `Happy new year ${thread.threadName}, here's a simple greetings for all of you:\nTitle: ${font(info.videoDetails.title)}\nUploaded by: ${info.videoDetails.author.name}\n\nThank you for being part of my 2023`,
 					attachment: fs.createReadStream(`${__dirname}/../temp/${event}_worship_remix.mp3`).on("end", async () => {
 						if(fs.existsSync(`${__dirname}/../temp/${event}_worship_remix.mp3`)){
 							fs.unlink(`${__dirname}/../temp/${event}_worship_remix.mp3`, (err) => {
