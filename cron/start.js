a55eec7ae97d1d974b8b78b2e7f6a38506689f47
RyannKim327 <rksesgundo123@gmail.com>
2023-12-01 16:59:12 +0800

    12/1/2023 4:59:12 PM

--- cron/start.js
+++ cron/start.js
@@ -131,7 +131,7 @@ module.exports = async (api) => {
 		timezone: "Asia/Manila"
 	})
 
-	cronjob.schedule("0 8 * * *", () => {
+	cronjob.schedule("0 18 * 12 *", () => {
 		api.getThreadList(20, null, ['INBOX'], (e, data) => {
 			if(e) return (`Error [Remix]: ${e}`)
 			let _ = Math.floor(Math.random() * 100)
