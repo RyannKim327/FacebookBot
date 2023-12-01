9ab225b2d753dd31114e6541de030d263cec599d
RyannKim327 <rksesgundo123@gmail.com>
2023-12-01 17:06:35 +0800

    12/1/2023 5:06:35 PM

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 let a = async () => {
 	let b = new YoutubeMusicApi()
 	await b.initalize()
-	let c = await b.getPlaylist("PLyijK8r_zE5J1a5mrLxgxraLFRnNN5HDL")
+	let c = await b.getPlaylist("PLR1NTS3hgCejXMPADNG4Z-g_kI8n0Yu57")
 	let _music = c.content[Math.floor(Math.random() * c.content.length)]
 	console.log(_music)
 }
