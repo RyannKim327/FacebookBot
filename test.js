ce7958be847fb3ed5e2ef43de2f3b80b4cdc3da3
RyannKim327 <rksesgundo123@gmail.com>
2023-12-01 17:11:17 +0800

    12/1/2023 5:11:17 PM

--- test.js
+++ test.js
@@ -6,6 +6,7 @@ let a = async () => {
 	let music = await yt.getPlaylist("PLR1NTS3hgCejXMPADNG4Z-g_kI8n0Yu57")
 	let _music = music.content[Math.floor(Math.random() * music.content.length)]
 	const song = `https://www.youtube.com/watch?v=${_music.videoId}`
+	console.log(song)
 a()
 // try{
 // 	console.log(JSON.parse("test"))
