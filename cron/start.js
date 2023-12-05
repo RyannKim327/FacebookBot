ce00531283c907f3fcde7ecff8a2695deac18155
RyannKim327 <rksesgundo123@gmail.com>
2023-12-05 21:11:00 +0800

    12/5/2023 9:10:59 PM

--- cron/start.js
+++ cron/start.js
@@ -126,7 +126,7 @@ module.exports = async (api) => {
 		timezone: "Asia/Manila"
 	})
 
-	cronjob.schedule("0 18 0 12 *", () => {
+	cronjob.schedule("0 18 * 12 *", () => {
 		api.getThreadList(20, null, ['INBOX'], (e, data) => {
 			if(e) return (`Error [Pasko]: ${e}`)
 			data.forEach(r => {
