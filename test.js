209a3ed53ff68cab370631918a8939e4240443bc
RyannKim327 <rksesgundo123@gmail.com>
2023-12-11 00:40:16 +0800

    12/11/2023 12:40:15 AM Focus muna ako sa programming

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
-const YoutubeMusicApi = require("youtube-music-api");
+// const YoutubeMusicApi = require("youtube-music-api");
 
-let a = async () => {
-	let yt = new YoutubeMusicApi()
-	await yt.initalize()
-	let music = await yt.getPlaylist("PLR1NTS3hgCejXMPADNG4Z-g_kI8n0Yu57")
-	let _music = music.content[Math.floor(Math.random() * music.content.length)]
-	const song = `https://www.youtube.com/watch?v=${_music.videoId}`
-	console.log(song)
-}
-a()
+// let a = async () => {
+// 	let yt = new YoutubeMusicApi()
+// 	await yt.initalize()
+// 	let music = await yt.getPlaylist("PLR1NTS3hgCejXMPADNG4Z-g_kI8n0Yu57")
+// 	let _music = music.content[Math.floor(Math.random() * music.content.length)]
+// 	const song = `https://www.youtube.com/watch?v=${_music.videoId}`
+// 	console.log(song)
+// }
+// a()
 // try{
 // 	console.log(JSON.parse("test"))
 // }catch(e){
