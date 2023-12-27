937ea51746f2e945281a035147d6b984a6e57ee3
RyannKim327 <rksesgundo123@gmail.com>
2023-12-28 05:06:26 +0800

    12-28-2023 5:6

--- config.js
+++ config.js
@@ -331,8 +331,8 @@ let listerner = async (api) => {
 				afkCalls[event.threadID] = undefined
 			}, ((1000 * 60) * 60))
 		}
-		if((event.body.toLowerCase().startsWith(name.toLowerCase()) && event.body.trim().toLowerCase() == name.toLowerCase())|| event.body.startsWith(prefix)){
-			if(trialCard[event.senderID] == undefined && !json.off.includes(event.senderID) && !calls.includes(event.senderID)){
+		if(trialCard[event.senderID] == undefined && !json.off.includes(event.senderID) && !calls.includes(event.senderID)){
+			if((event.body.toLowerCase().startsWith(name.toLowerCase()) && event.body.trim().toLowerCase() == name.toLowerCase())|| event.body.startsWith(prefix)){
 
 			}
 		}
