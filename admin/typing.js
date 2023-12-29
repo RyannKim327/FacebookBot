67f621a6457aec5d89720e35166c518e8c829634
RyannKim327 <rksesgundo123@gmail.com>
2023-12-29 16:27:26 +0800

    12-29-2023 16:27 [Test repl mode]

--- admin/typing.js
+++ admin/typing.js
@@ -2,7 +2,9 @@ module.exports = (api, event) => {
 	let i = 5
 	setInterval(async () => {
 		if(i > 0){
-			await api.sendTypingIndicator(event.threadID, (e) => {})
+			await api.sendTypingIndicator(event.threadID, (e) => {
+				console.log(e.message)
+			})
 		}
 	})
 }
