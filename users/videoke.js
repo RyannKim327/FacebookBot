054110bcb98f84c37093922fd04ac44c0bc2e126
RyannKim327 <weryses19@gmail.com>
2023-12-31 04:36:18 +0000

    12-31-23 12:36

--- users/videoke.js
+++ users/videoke.js
@@ -1,7 +1,8 @@
-const youtube = require("youtube-music-api")
+const YoutubeMusicApi = require("youtube-music-api")
+const yt = new YoutubeMusicApi()
 const http = require("https")
 const fs = require("fs")
-const ytdl = requrie("ytdl-core")
+const ytdl = require("ytdl-core")
 const ffmpeg = require("@ffmpeg-installer/ffmpeg")
 const ffmpegs = require("fluent-ffmpeg")
 ffmpegs.setFfmpegPath(ffmpeg.path)
@@ -9,10 +10,10 @@ ffmpegs.setFfmpegPath(ffmpeg.path)
 module.exports = async (api, event) => {
 	const playlist = "PLWzl3AM4OHkxyqK9-BEKefHMSRzwEs3Bf"
 	let filename = `${__dirname}/../temp/${event.threadID}_${event.senderID}_videoke.mp4`
-	const yt = new youtube()
 	const list = await yt.getPlaylist(playlist)
+	console.log(list)
 	const musics = list.content
-	const music = musics[Math.floor(Math.random() * musics.length]
+	const music = musics[Math.floor(Math.random() * musics.length)]
 	const url = `https://www.youtube.com/watch?v=${music.videoId}`
 	if(!fs.existsSync(filename)){
 		try{
@@ -20,16 +21,16 @@ module.exports = async (api, event) => {
 			const stream = ytdl(url, {
 				quality: "lowest"
 			})
-			const info = await ytdl.getInfo(url)
-			ffmpegs(stream).audioBitrate(96).save(filename)
-			api.sendMessage({
-				body: "Its your time to shine, kanta mo na to!",
-				attachment: fs.createReadStream(filename).on("end", async () => {
-					if(fs.existsSync(filename)){
-						fs.unlinkSync(filename, (err) => {})
-					}
-				})
-			}, event.threadID, (e, m) => {})
+			ffmpegs(stream).audioBitrate(48).save(filename).on("end", async () => {
+				api.sendMessage({
+					body: "Its your time to shine, kanta mo na to!",
+					attachment: fs.createReadStream(filename).on("end", async () => {
+						if(fs.existsSync(filename)){
+							fs.unlinkSync(filename, (err) => {})
+						}
+					})
+				}, event.threadID, (e, m) => {})
+			})
 		}catch(err) {
 			api.sendMessage(`Error: ${err.message}`, event.threadID, (e, m) => {})
 		}
