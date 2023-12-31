3a76f8d22962e5b6778c702b1a62e4c5a28f3f82
RyannKim327 <rksesgundo123@gmail.com>
2023-12-31 17:13:55 +0800

    12-31-2023 17:13 [User mode]

--- test2.js
+++ test2.js
@@ -1,8 +1,26 @@
 const playlists = [
-    "PLI3KQeEwuu89pAYq9wl4izyZ-9b1Njsog",
-    "PLWzl3AM4OHkxyqK9-BEKefHMSRzwEs3Bf",
-    "PLpL27IibkE6v1WHG-WrbskY-fFHdTK0sH",
-    "PLUfc10pMf7KRU0sFsv8d8m8APdkE9cuE2"
+    "PLI3KQeEwuu89pAYq9wl4izyZ-9b1Njsog"
+    // "PLWzl3AM4OHkxyqK9-BEKefHMSRzwEs3Bf",
+    // "PLpL27IibkE6v1WHG-WrbskY-fFHdTK0sH",
+    // "PLUfc10pMf7KRU0sFsv8d8m8APdkE9cuE2"
 ]
 const playlist = playlists[Math.floor(Math.random() * playlists.length)]
 console.log(playlist)
+
+const youtube = require("youtube-music-api")
+const yt = new youtube()
+async function run(){
+    await yt.initalize()
+    const music = await yt.getPlaylist(playlist)
+    console.log(music)
+    // let _music = music.content[Math.floor(Math.random() * music.content.length)]
+	// while(_music.videoId == undefined){
+	// 	_music = music.content[Math.floor(Math.random() * music.content.length)]
+	// }
+	// while(_music.videoId == null){
+	// 	_music = music.content[Math.floor(Math.random() * music.content.length)]
+	// }
+	// const url = `https://www.youtube.com/watch?v=${_music.videoId}`
+    // console.log(url)
+}
+run()
--- users/videoke.js
+++ users/videoke.js
@@ -10,25 +10,12 @@ const afk = require("./../utils/afk")
 const gender = require("./../utils/gender")
 
 module.exports = async (api, event) => {
-	const playlists = [
-		"PLI3KQeEwuu89pAYq9wl4izyZ-9b1Njsog",
-		"PLWzl3AM4OHkxyqK9-BEKefHMSRzwEs3Bf",
-		"PLpL27IibkE6v1WHG-WrbskY-fFHdTK0sH",
-		"PLUfc10pMf7KRU0sFsv8d8m8APdkE9cuE2"
-	]
-	const playlist = playlists[Math.floor(Math.random() * playlists.length)]
-	console.log(playlist)
+	const playlist = "PLWzl3AM4OHkxyqK9-BEKefHMSRzwEs3Bf"
 	let name = `${__dirname}/../temp/${event.threadID}_${event.senderID}_videoke.mp4`
 	let json2 = JSON.parse(fs.readFileSync("data/preferences.json", "utf8"))
 	await yt.initalize()
 	let music = await yt.getPlaylist(playlist)
 	let _music = music.content[Math.floor(Math.random() * music.content.length)]
-	while(_music.videoId == undefined){
-		_music = music.content[Math.floor(Math.random() * music.content.length)]
-	}
-	while(_music.videoId == null){
-		_music = music.content[Math.floor(Math.random() * music.content.length)]
-	}
 	const url = `https://www.youtube.com/watch?v=${_music.videoId}`
 	if(!fs.existsSync(name)){
 		try{
