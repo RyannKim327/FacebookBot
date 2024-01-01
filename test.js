0c387de1a76662f2a8c1c5d4aa6954b56ec6ae5d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-01 11:49:34 +0800

    1-1-2024 11:49 [User mode]

--- test.js
+++ test.js
@@ -17,33 +17,52 @@
 const fs = require("fs")
 const { exec } = require("child_process")
 const date = require("./utils/date")
+const axios = require("axios")
 
-let commits = 100
+let commits = 150
 
 let run = async () => {
 	const time = date("Asia/Manila")
 	const m = `${time.getMonth() + 1}-${time.getDate()}-${time.getFullYear()} ${time.getHours()}:${time.getMinutes()}`
+	// const m2 = [
+	// 	"Sana ako pa rin",
+	// 	"Bakit na mimiss ko pa rin sya",
+	// 	"Ano kaya ang susunod na ilalagay ko dito",
+	// 	"From valentines, hanggang pasko, na sana pati bagong taon",
+	// 	"Ang cute nya, lalo na pag inis sya saken",
+	// 	"Wala, talaga lang attractive sya saken",
+	// 	"Nugagawen kapag miss ko na sya?",
+	// 	"Parang malabo na pala, di na ko aasa",
+	// 	"Mahirap lang isipin, pero tanggap ko.",
+	// 	"Kahit anong gawin prii, sya pa rin."
+	// ]
 	const m2 = [
-		"Sana ako pa rin",
-		"Bakit na mimiss ko pa rin sya",
-		"Ano kaya ang susunod na ilalagay ko dito",
-		"From valentines, hanggang pasko, na sana pati bagong taon",
-		"Ang cute nya, lalo na pag inis sya saken",
-		"Wala, talaga lang attractive sya saken",
-		"Nugagawen kapag miss ko na sya?",
-		"Parang malabo na pala, di na ko aasa",
-		"Mahirap lang isipin, pero tanggap ko.",
-		"Kahit anong gawin prii, sya pa rin."
-	]
-
+		"a random javascript code that has input",
+		"a random javascript code that uses fetch",
+		"a random javascript code that uses axios",
+		"a random javascript code that uses cron"
+	]	
 	const m3 = m2[Math.floor(Math.random() * m2.length)]
+	let { data } = await axios.get(`https://hercai.onrender.com/v3-beta/hercai?question=${m3}`)
 
-	console.log("Restating")
+	let datas = data.reply.split("\n")
+	let result = ""
+	let active = false
+
+	for(let i = 0; i < datas.length; i++){
+		if(datas[i].startsWith("```")){
+			active = !active
+		}
+		if(active && !datas[i].startsWith("```")){
+			result += datas[i] + "\n"
+		}
+	}
+
 
 	// exec("git config --global user.name \"RyannKim327\"", (e) => {console.error(e)})
 	// exec("git config --global user.email \"rksesgundo123@gmail.com\"", (e) => {console.error(e)})
 
-	fs.writeFileSync("Auto git.txt", m3, "utf-8")
+	fs.writeFileSync("autogit.js", result, "utf-8")
 	setTimeout(() => {
 		console.log("Git add")
 		exec("git add .", (e) => {
