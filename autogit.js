5878599badf40e0979547a8122b652c43511a4b0
RyannKim327 <rksesgundo123@gmail.com>
2024-01-01 11:59:33 +0800

    1-1-2024 11:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,19 @@
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
+function bubbleSort(arr) {
+  var len = arr.length;
+  for (var i = 0; i < len; i++) {
+    for (var j = 0; j < len - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap the elements
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// Usage
+var unsortedArray = [5, 2, 6, 8, 1];
+var sortedArray = bubbleSort(unsortedArray);
+console.log(sortedArray);
