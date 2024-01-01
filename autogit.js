ca12ff45f047b1c2708e822fbb3f5d61918f7576
RyannKim327 <rksesgundo123@gmail.com>
2024-01-01 11:50:03 +0800

    1-1-2024 11:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,19 @@
-var userInput = prompt("Enter a message:"); // User input prompt
+const cron = require('cron');
 
-console.log("You entered: " + userInput); // Display user input on the console
+// Define a cron job schedule
+const job = new cron.CronJob('*/5 * * * * *', () => {
+  console.log('This job runs every 5 seconds');
+});
+
+// Start the cron job
+job.start();
+
+// Print a message when the job is stopped
+job.on('stop', () => {
+  console.log('Job stopped');
+});
+
+// Stop the cron job after 60 seconds
+setTimeout(() => {
+  job.stop();
+}, 60000);
