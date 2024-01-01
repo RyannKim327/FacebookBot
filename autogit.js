39c29ba512944f9a69c4dc01d32a5d35c324f288
RyannKim327 <rksesgundo123@gmail.com>
2024-01-01 11:58:53 +0800

    1-1-2024 11:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,21 @@
 const cron = require('node-cron');
 
-// Schedule a task to run every minute
-cron.schedule('* * * * *', () => {
-  console.log('Running cron job');
-  // Add your code here to perform the desired task
+// Schedule a cron job to run every 1 minute
+const task = cron.schedule('* * * * *', () => {
+  console.log('This task runs every minute');
 });
+
+// Schedule a cron job to run every weekday at 9:00 AM
+const morningTask = cron.schedule('0 9 * * 1-5', () => {
+  console.log('Good morning!');
+}, {
+  scheduled: true,
+  timezone: 'America/New_York'
+});
+
+// Stop the first task after 5 minutes
+setTimeout(() => {
+  task.stop();
+  console.log('Task has been stopped');
+}, 5 * 60 * 1000);
+npm install node-cron
