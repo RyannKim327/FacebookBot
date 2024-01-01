63a2e80956955c492e3eda8e98cb2929a728f607
RyannKim327 <rksesgundo123@gmail.com>
2024-01-01 12:08:51 +0800

    1-1-2024 12:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,46 @@
-function bubbleSort(arr) {
-  var len = arr.length;
-  for (var i = 0; i < len; i++) {
-    for (var j = 0; j < len - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        // Swap the elements
-        var temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-  return arr;
 }
 
-// Usage
-var unsortedArray = [5, 2, 6, 8, 1];
-var sortedArray = bubbleSort(unsortedArray);
-console.log(sortedArray);
+function reverseLinkedList(head) {
+  let prev = null;
+  let current = head;
+  let next = null;
+
+  while (current !== null) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+
+  return prev;
+}
+
+// Example usage:
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(4);
+
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+
+console.log("Original linked list:");
+let current = node1;
+while (current !== null) {
+  console.log(current.value);
+  current = current.next;
+}
+
+const reversed = reverseLinkedList(node1);
+console.log("Reversed linked list:");
+current = reversed;
+while (current !== null) {
+  console.log(current.value);
+  current = current.next;
+}
