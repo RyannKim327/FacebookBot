f7c1b97299112d24c3f9e309cef489f8433e53b1
RyannKim327 <weryses19@gmail.com>
2024-01-06 01:23:36 +0800

    1-6-2024 1:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,94 +1,20 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-    this.height = 1;
-  }
-}
-class AVLTree {
-  constructor() {
-    this.root = null;
-  }
-
-  // Other tree operations will be implemented here
-}
-function height(node) {
-  if (node === null) return 0;
-  return node.height;
-}
-function updateHeight(node) {
-  node.height = Math.max(height(node.left), height(node.right)) + 1;
-}
-function leftRotate(node) {
-  const newRoot = node.right;
-  node.right = newRoot.left;
-  newRoot.left = node;
-
-  updateHeight(node);
-  updateHeight(newRoot);
-
-  return newRoot;
-}
-function rightRotate(node) {
-  const newRoot = node.left;
-  node.left = newRoot.right;
-  newRoot.right = node;
-
-  updateHeight(node);
-  updateHeight(newRoot);
-
-  return newRoot;
-}
-function balanceFactor(node) {
-  return height(node.left) - height(node.right);
-}
-function insert(value, root) {
-  if (root === null) {
-    return new Node(value);
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let key = array[i];
+    let j = i - 1;
+    
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    
+    array[j + 1] = key;
   }
 
-  if (value < root.value) {
-    root.left = insert(value, root.left);
-  } else if (value > root.value) {
-    root.right = insert(value, root.right);
-  } else {
-    // Duplicate values are not allowed in an AVL tree
-    return root;
-  }
-
-  updateHeight(root);
-
-  const balance = balanceFactor(root);
-
-  // Left-Left case
-  if (balance > 1 && value < root.left.value) {
-    return rightRotate(root);
-  }
-
-  // Right-Right case
-  if (balance < -1 && value > root.right.value) {
-    return leftRotate(root);
-  }
-
-  // Left-Right case
-  if (balance > 1 && value > root.left.value) {
-    root.left = leftRotate(root.left);
-    return rightRotate(root);
-  }
-
-  // Right-Left case
-  if (balance < -1 && value < root.right.value) {
-    root.right = rightRotate(root.right);
-    return leftRotate(root);
-  }
-
-  return root;
+  return array;
 }
-const avlTree = new AVLTree();
 
-avlTree.root = insert(10, avlTree.root);
-avlTree.root = insert(20, avlTree.root);
-avlTree.root = insert(30, avlTree.root);
-avlTree.root = insert(40, avlTree.root);
-avlTree.root = insert(50, avlTree.root);
+// Example usage:
+const unsortedArray = [4, 2, 6, 8, 1, 3, 5, 7];
+const sortedArray = insertionSort(unsortedArray);
+console.log(sortedArray);
