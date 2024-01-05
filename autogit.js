cc64d2810dbcaf29ceed94937c07a4477086894a
RyannKim327 <weryses19@gmail.com>
2024-01-05 19:01:26 +0800

    1-5-2024 19:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,44 @@
-const str = "Hello World!";
-
-// Reverse the string using the reverse() method
-const reversedStr = str.split(' ').reverse().join(' ');
-
-console.log(reversedStr); // Output: "!dlroW olleH"
-const str = "Hello World!";
-
-// Initialize an empty string to store the reversed string
-let reversedStr = "";
-
-// Iterate over the string from the end to the beginning
-for (let i = str.length - 1; i >= 0; i--) {
-  // Add the current character to the reversed string
-  reversedStr += str[i];
+function isPalindrome(str) {
+  const reversedStr = str.split('').reverse().join('');
+  return str === reversedStr;
 }
 
-console.log(reversedStr); // Output: "!dlroW olleH"
-const str = "Hello World!";
+console.log(isPalindrome('racecar')); // true
+console.log(isPalindrome('hello')); // false
+function isPalindrome(str) {
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
 
-// Reverse the string using the reduce() method
-const reversedStr = str.split(' ').reduce((acc, curr) => curr + ' ' + acc, '');
+console.log(isPalindrome('racecar')); // true
+console.log(isPalindrome('hello')); // false
+function isPalindrome(str, start = 0, end = str.length - 1) {
+  if (start >= end) {
+    return true;
+  }
+  if (str[start] !== str[end]) {
+    return false;
+  }
+  return isPalindrome(str, start + 1, end - 1);
+}
 
-console.log(reversedStr); // Output: "!dlroW olleH"
+console.log(isPalindrome('racecar')); // true
+console.log(isPalindrome('hello')); // false
+function isPalindrome(str) {
+  return str === str.split('').reverse().join('');
+}
+
+console.log(isPalindrome('racecar')); // true
+console.log(isPalindrome('hello')); // false
+function isPalindrome(str) {
+  const reversedStr = parseInt(str.split('').reverse().join(''), 2);
+  const originalStr = parseInt(str, 2);
+  return reversedStr === originalStr;
+}
+
+console.log(isPalindrome('11001100')); // true
+console.log(isPalindrome('10101011')); // false
