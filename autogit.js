2884f5766eea4cb866ed11c0952d958dbd0478c6
RyannKim327 <weryses19@gmail.com>
2024-01-05 19:13:43 +0800

    1-5-2024 19:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,20 @@
-function isPalindrome(head) {
-  // Find the middle node of the linked list
+function findMiddleElement(head) {
   let slow = head;
   let fast = head;
-  while (fast && fast.next) {
+
+  while (fast !== null && fast.next !== null) {
     slow = slow.next;
     fast = fast.next.next;
   }
 
-  // Reverse the second half of the linked list
-  let reversedHead = null;
-  while (slow) {
-    const next = slow.next;
-    slow.next = reversedHead;
-    reversedHead = slow;
-    slow = next;
-  }
-
-  // Compare the first half and the second half of the linked list
-  let firstHalf = head;
-  let secondHalf = reversedHead;
-  while (firstHalf && secondHalf) {
-    if (firstHalf.val !== secondHalf.val) {
-      return false;
-    }
-    firstHalf = firstHalf.next;
-    secondHalf = secondHalf.next;
-  }
-
-  // Restore the original linked list
-  slow = reversedHead;
-  reversedHead = null;
-  while (slow) {
-    const next = slow.next;
-    slow.next = reversedHead;
-    reversedHead = slow;
-    slow = next;
-  }
-
-  // Return the result
-  return true;
+  return slow;
 }
+let head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+head.next.next.next.next = new ListNode(5);
+
+let middleElement = findMiddleElement(head);
+
+console.log(middleElement.val); // Output: 3
