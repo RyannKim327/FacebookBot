14ac48df63de8ab7c7e0efb34de2852a8a0c1adf
RyannKim327 <weryses19@gmail.com>
2024-01-06 01:44:18 +0800

    1-6-2024 1:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,54 @@
-function calculateTriangleArea(base, height) {
-  return (base * height) / 2;
-}
+function createKMPTable(pattern) {
+  const table = Array(pattern.length).fill(0);
 
-// Example usage:
-var base = 4;
-var height = 6;
-var area = calculateTriangleArea(base, height);
-console.log('The area of the triangle is: ' + area);
+  let prefixIndex = 0;
+  let suffixIndex = 1;
+
+  while (suffixIndex < pattern.length) {
+    if (pattern[prefixIndex] === pattern[suffixIndex]) {
+      table[suffixIndex] = prefixIndex + 1;
+      prefixIndex++;
+      suffixIndex++;
+    } else if (prefixIndex !== 0) {
+      prefixIndex = table[prefixIndex - 1];
+    } else {
+      table[suffixIndex] = 0;
+      suffixIndex++;
+    }
+  }
+
+  return table;
+}
+function searchByText(text, pattern) {
+  const table = createKMPTable(pattern);
+
+  let textIndex = 0;
+  let patternIndex = 0;
+  const indexes = [];
+
+  while (textIndex < text.length) {
+    if (text[textIndex] === pattern[patternIndex]) {
+      textIndex++;
+      patternIndex++;
+    }
+
+    if (patternIndex === pattern.length) {
+      indexes.push(textIndex - patternIndex);
+      patternIndex = table[patternIndex - 1];
+    } else if (textIndex < text.length && text[textIndex] !== pattern[patternIndex]) {
+      if (patternIndex !== 0) {
+        patternIndex = table[patternIndex - 1];
+      } else {
+        textIndex++;
+      }
+    }
+  }
+
+  return indexes;
+}
+const text = "ABABDABACDABABCABAB";
+const pattern = "ABABCABAB";
+
+const indexes = searchByText(text, pattern);
+
+console.log(indexes); // Output: [10]
