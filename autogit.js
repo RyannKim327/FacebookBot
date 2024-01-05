29a71561527882592e382db4e2748450658d5e29
RyannKim327 <weryses19@gmail.com>
2024-01-05 19:17:57 +0800

    1-5-2024 19:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,55 @@
-const removeVowels = (str) => {
-  return str.replace(/[aeiou]/gi, '');
-};
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-// Example usage
-const str = 'Hello World!';
-console.log(removeVowels(str)); // 'Hll Wrld!'
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  add(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  reverse() {
+    let currentNode = this.head;
+    let previousNode = null;
+
+    while (currentNode) {
+      const nextNode = currentNode.next;
+      currentNode.next = previousNode;
+      previousNode = currentNode;
+      currentNode = nextNode;
+    }
+
+    this.head = previousNode;
+    this.tail = currentNode;
+
+    return this.head;
+  }
+}
+
+// Example Usage
+const linkedList = new LinkedList();
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(3);
+linkedList.add(4);
+linkedList.add(5);
+
+console.log("Original Linked List:");
+console.log(linkedList);
+
+console.log("Reversed Linked List:");
+console.log(linkedList.reverse());
