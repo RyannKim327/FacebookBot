2bc3cc569a3755801ad1cc9684423308b5e18874
RyannKim327 <weryses19@gmail.com>
2024-01-05 19:29:31 +0800

    1-5-2024 19:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,33 @@
-function isPrime(num) {
-  if (num <= 1) {
-    return false;
+class Node {
+  constructor(keys, children) {
+    this.keys = keys || [];
+    this.children = children || [];
   }
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
-      return false;
+}
+
+class BTree {
+  constructor(order) {
+    this.root = new Node();
+    this.order = order;
+  }
+
+  insert(key, value) {
+    let node = this.root;
+    while (true) {
+      const index = node.keys.findIndex(k => k >= key);
+      if (index >= 0) {
+        // Key already exists, update value
+        node.children[index].value = value;
+        return;
+      }
+      if (node.children.length === 0) {
+        // Leaf node, insert key-value pair
+        node.keys.push(key);
+        node.children.push({ key, value });
+        return;
+      } else {
+        node = node.children[index];
+      }
     }
   }
-  return true;
 }
