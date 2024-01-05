b2b269f10391962392f2a17ec5b49ec69458d794
RyannKim327 <weryses19@gmail.com>
2024-01-06 02:11:26 +0800

    1-6-2024 2:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,53 @@
-function bubbleSort(arr) {
-  var len = arr.length;
-  for (var i = 0; i < len - 1; i++) {
-    for (var j = 0; j < len - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        var temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
+class Stack {
+  constructor() {
+    this.stack = [];
   }
-  return arr;
+
+  // Add an element to the top of the stack
+  push(element) {
+    this.stack.push(element);
+  }
+
+  // Remove and return the top element from the stack
+  pop() {
+    if (this.isEmpty()) {
+      return "Stack is empty";
+    }
+    return this.stack.pop();
+  }
+
+  // Return the top element without removing it from the stack
+  peek() {
+    if (this.isEmpty()) {
+      return "Stack is empty";
+    }
+    return this.stack[this.stack.length - 1];
+  }
+
+  // Check if the stack is empty
+  isEmpty() {
+    return this.stack.length === 0;
+  }
+
+  // Return the size of the stack
+  size() {
+    return this.stack.length;
+  }
+
+  // Print the stack elements
+  print() {
+    console.log(this.stack);
+  }
 }
 
-// Testing the algorithm
-var unsortedArray = [5, 3, 8, 2, 1, 4];
-var sortedArray = bubbleSort(unsortedArray);
-console.log(sortedArray);
+// Example usage:
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.print(); // Output: [1, 2, 3]
+console.log(stack.pop()); // Output: 3
+console.log(stack.peek()); // Output: 2
+console.log(stack.size()); // Output: 2
+console.log(stack.isEmpty()); // Output: false
+stack.print(); // Output: [1, 2]
