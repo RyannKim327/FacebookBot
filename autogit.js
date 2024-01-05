0e3f4a1b31940454305c4848552f050de97c764a
RyannKim327 <weryses19@gmail.com>
2024-01-05 22:58:47 +0800

    1-5-2024 22:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,10 @@
-function lcs(str1, str2) {
-  const m = str1.length;
-  const n = str2.length;
-  const lcs = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
-
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        lcs[i][j] = lcs[i - 1][j - 1] + 1;
-      } else {
-        lcs[i][j] = Math.max(lcs[i - 1][j], lcs[i][j - 1]);
-      }
-    }
+function factorial(num) {
+  // Base case: Factorial of 0 is 1
+  if (num === 0) {
+    return 1;
   }
-
-  let lcsStr = "";
-  let i = m;
-  let j = n;
-  while (i > 0 && j > 0) {
-    if (str1[i - 1] === str2[j - 1]) {
-      lcsStr = str1[i - 1] + lcsStr;
-      i--;
-      j--;
-    } else if (lcs[i - 1][j] > lcs[i][j - 1]) {
-      i--;
-    } else {
-      j--;
-    }
+  // Recursive case: Multiply num by the factorial of num-1
+  else {
+    return num * factorial(num - 1);
   }
-
-  return lcsStr;
 }
-const str1 = "ABCDGH";
-const str2 = "AEDFHR";
-const lcsStr = lcs(str1, str2);
-console.log(lcsStr); // "ADH"
