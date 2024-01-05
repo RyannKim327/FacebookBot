0eb0e83594a6513215015165d6a9a40fdd0d5d4d
RyannKim327 <weryses19@gmail.com>
2024-01-05 19:05:01 +0800

    1-5-2024 19:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,67 @@
-let string1 = "Hello";
-let string2 = "World";
-let concatenatedString = string1 + string2;
+// Node class
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-console.log(concatenatedString); // Output: "HelloWorld"
-let string1 = "Hello";
-let string2 = "World";
-let concatenatedString = string1.concat(string2);
+// Linked list class
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
 
-console.log(concatenatedString); // Output: "HelloWorld"
-let string1 = "Hello";
-let string2 = "World";
+  // Add a node to the end of the list
+  add(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.size++;
+  }
 
-string1 += string2;
+  // Find the nth node from the end of the list
+  findNthFromEnd(n) {
+    // Check if the index is valid
+    if (n <= 0 || n > this.size) {
+      throw new Error("Invalid index");
+    }
 
-console.log(string1); // Output: "HelloWorld"
+    // Find the length of the list
+    let length = this.size;
+
+    // Calculate the index of the nth node from the start of the list
+    const index = length - n;
+
+    // Traverse the list from the start, counting the nodes until you reach the nth node
+    let current = this.head;
+    let count = 0;
+    while (current) {
+      if (count === index) {
+        return current.data;
+      }
+      current = current.next;
+      count++;
+    }
+  }
+}
+
+// Create a linked list
+const list = new LinkedList();
+list.add(1);
+list.add(2);
+list.add(3);
+list.add(4);
+list.add(5);
+
+// Find the 2nd node from the end of the list
+const nthNode = list.findNthFromEnd(2);
+
+console.log(nthNode); // Output: 4
