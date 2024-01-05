1e3e9c6e3ad7ce43140d3e9055bb7fae562c06d3
RyannKim327 <weryses19@gmail.com>
2024-01-05 23:37:10 +0800

    1-5-2024 23:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,43 @@
-function selectionSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    // Find the minimum element in the unsorted part of the array
-    let minIndex = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
-    }
+const arr = [1, 2, 3, 4, 5, 1, 2, 3];
 
-    // Swap the minimum element with the first element of the unsorted part
-    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+const uniqueArr = Array.from(new Set(arr));
+
+console.log(uniqueArr); // Output: [1, 2, 3, 4, 5]
+const arr = [1, 2, 3, 4, 5, 1, 2, 3];
+
+const uniqueArr = arr.filter((item, index) => {
+  return arr.indexOf(item) === index;
+});
+
+console.log(uniqueArr); // Output: [1, 2, 3, 4, 5]
+const arr = [1, 2, 3, 4, 5, 1, 2, 3];
+
+const uniqueArr = arr.reduce((accumulator, currentValue) => {
+  if (!accumulator[currentValue]) {
+    accumulator[currentValue] = true;
+    return [...accumulator, currentValue];
+  } else {
+    return accumulator;
   }
+}, []);
 
-  return arr;
-}
+console.log(uniqueArr); // Output: [1, 2, 3, 4, 5]
+const arr = [1, 2, 3, 4, 5, 1, 2, 3];
+
+const uniqueArr = [];
+
+arr.forEach((item) => {
+  if (arr.indexOf(item) === arr.lastIndexOf(item)) {
+    uniqueArr.push(item);
+  }
+});
+
+console.log(uniqueArr); // Output: [1, 2, 3, 4, 5]
+const arr = [1, 2, 3, 4, 5, 1, 2, 3];
+
+const uniqueArr = Object.values(arr.reduce((accumulator, currentValue) => {
+  accumulator[currentValue] = true;
+  return accumulator;
+}, {}));
+
+console.log(uniqueArr); // Output: [1, 2, 3, 4, 5]
