9cd0968c756dbe0ecf9a5761eb0a2b1f9a25ba1e
RyannKim327 <weryses19@gmail.com>
2024-01-06 02:09:02 +0800

    1-6-2024 2:8 [User mode]

--- autogit.js
+++
@@ -1,8 +0,0 @@
-function decimalToBinary(decimalNum) {
-  return decimalNum.toString(2);
-}
-
-// Example usage
-const decimalNumber = 10;
-const binaryNumber = decimalToBinary(decimalNumber);
-console.log(binaryNumber); // Output: 1010
