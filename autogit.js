8c6dd4fb90ecef2a490848b086d8d27764104976
RyannKim327 <weryses19@gmail.com>
2024-01-05 19:14:56 +0800

    1-5-2024 19:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,49 @@
-function countCharacter(string, target) {
-  let count = 0;
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] === target) {
-      count++;
+function isAnagram(str1, str2) {
+  return str1.toLowerCase().split('').sort().join('') === str2.toLowerCase().split('').sort().join('');
+}
+function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  let charMap = new Map();
+
+  for (let char of str1) {
+    if (charMap.has(char)) {
+      charMap.set(char, charMap.get(char) + 1);
+    } else {
+      charMap.set(char, 1);
     }
   }
-  return count;
-}
 
-console.log(countCharacter("Hello world", "l")); // 3
-console.log(countCharacter("Mississippi", "s")); // 4
-console.log(countCharacter("abracadabra", "a")); // 5
+  for (let char of str2) {
+    if (!charMap.has(char) || charMap.get(char) === 0) {
+      return false;
+    } else {
+      charMap.set(char, charMap.get(char) - 1);
+    }
+  }
+
+  return true;
+}
+function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  let charCounts = new Array(256).fill(0);
+
+  for (let char of str1) {
+    charCounts[char.charCodeAt(0)]++;
+  }
+
+  for (let char of str2) {
+    if (charCounts[char.charCodeAt(0)] === 0) {
+      return false;
+    } else {
+      charCounts[char.charCodeAt(0)]--;
+    }
+  }
+
+  return true;
+}
