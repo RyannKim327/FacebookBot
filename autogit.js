4b4ed1c83e90846d8ff7e703d074133c0b64f5cf
RyannKim327 <weryses19@gmail.com>
2024-01-06 01:18:34 +0800

    1-6-2024 1:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,65 @@
-class Graph {
-  constructor() {
-    this.vertices = {};
-  }
-
-  addVertex(vertex) {
-    this.vertices[vertex] = [];
-  }
-
-  addEdge(vertex1, vertex2) {
-    this.vertices[vertex1].push(vertex2);
-    this.vertices[vertex2].push(vertex1);
-  }
-
-  breadthFirstSearch(startingVertex) {
-    const visited = new Set();
-    const queue = [startingVertex];
-
-    while (queue.length > 0) {
-      const currentVertex = queue.shift();
-      console.log(currentVertex);
-      visited.add(currentVertex);
-
-      const adjacentVertices = this.vertices[currentVertex];
-      for (let i = 0; i < adjacentVertices.length; i++) {
-        const vertex = adjacentVertices[i];
-        if (!visited.has(vertex)) {
-          queue.push(vertex);
-          visited.add(vertex);
-        }
-      }
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
 
-// Example usage
-const graph = new Graph();
-graph.addVertex(1);
-graph.addVertex(2);
-graph.addVertex(3);
-graph.addVertex(4);
-graph.addEdge(1, 2);
-graph.addEdge(2, 3);
-graph.addEdge(3, 4);
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
 
-graph.breadthFirstSearch(1);
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.size++;
+  }
+
+  dequeue() {
+    if (!this.head) {
+      return null;
+    }
+    const value = this.head.value;
+    this.head = this.head.next;
+    this.size--;
+    if (!this.head) {
+      this.tail = null;
+    }
+    return value;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getFront() {
+    return this.head ? this.head.value : null;
+  }
+
+  getSize() {
+    return this.size;
+  }
+}
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+
+console.log(queue.getFront()); // Output: 1
+console.log(queue.getSize()); // Output: 3
+
+console.log(queue.dequeue()); // Output: 1
+
+console.log(queue.getFront()); // Output: 2
+console.log(queue.getSize()); // Output: 2
+
+console.log(queue.isEmpty()); // Output: false
