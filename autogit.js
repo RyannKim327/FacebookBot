31431be08942508722d06b734de41140e6149965
RyannKim327 <weryses19@gmail.com>
2024-01-06 02:13:30 +0800

    1-6-2024 2:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,6 @@
-function insertionSort(array) {
-  for (let i = 1; i < array.length; i++) {
-    let current = array[i];
-    let j = i - 1;
-    
-    while (j >= 0 && array[j] > current) {
-      array[j + 1] = array[j];
-      j--;
-    }
-    
-    array[j + 1] = current;
-  }
-  
-  return array;
-}
+let str = "Hello World!";
 
-// Test the insertionSort function
-const unsortedArray = [5, 2, 8, 3, 1];
-const sortedArray = insertionSort(unsortedArray);
-console.log(sortedArray); // Output: [1, 2, 3, 5, 8]
+// Using the replace() method with a regular expression to remove spaces
+let newStr = str.replace(/\s+/g, "");
+
+console.log(newStr); // Output: "HelloWorld!"
