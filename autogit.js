7770e92953ed058200e80683197f3f30f0dde884
RyannKim327 <weryses19@gmail.com>
2024-01-05 23:17:21 +0800

    1-5-2024 23:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,16 @@
-function breadthFirstSearch(graph, startNode) {
-  // Create a queue and enqueue the starting node
-  let queue = [startNode];
-
-  // Create a set to keep track of visited nodes
-  let visited = new Set();
-
-  // While the queue is not empty
-  while (queue.length > 0) {
-
-    // Dequeue a node from the queue
-    let currentNode = queue.shift();
-
-    // Mark the node as visited
-    visited.add(currentNode);
-
-    // Explore the node's neighbors
-    for (let neighbor of graph[currentNode]) {
-      if (!visited.has(neighbor)) {
-        // Enqueue the neighbor if it has not been visited
-        queue.push(neighbor);
-      }
-    }
+function findLength(string) {
+  let length = 0;
+  for (let i = 0; i < string.length; i++) {
+    length++;
   }
-
-  // Return the visited nodes
-  return visited;
+  return length;
+}
+function findLength(string) {
+  if (string === "") {
+    return 0;
+  }
+  return 1 + findLength(string.slice(1));
+}
+function findLength(string) {
+  return string.split("").reduce((acc, _) => acc + 1, 0);
 }
