90dc60e2bc091cf53de54234d05b162d22fbcdd0
RyannKim327 <weryses19@gmail.com>
2024-01-05 19:13:19 +0800

    1-5-2024 19:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,42 @@
-// Import the necessary modules.
-const fetch = require('node-fetch');
-const fs = require('fs');
+function isPalindrome(head) {
+  // Find the middle node of the linked list
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
 
-// Define the URL of the API endpoint.
-const endpoint = 'https://example.com/api/v1/endpoint';
+  // Reverse the second half of the linked list
+  let reversedHead = null;
+  while (slow) {
+    const next = slow.next;
+    slow.next = reversedHead;
+    reversedHead = slow;
+    slow = next;
+  }
 
-// Make a GET request to the API endpoint.
-fetch(endpoint)
-  .then(response => response.json())
-  .then(data => {
-    // Save the data to a file.
-    fs.writeFileSync('data.json', JSON.stringify(data));
-  })
-  .catch(error => {
-    console.error(error);
-  });
+  // Compare the first half and the second half of the linked list
+  let firstHalf = head;
+  let secondHalf = reversedHead;
+  while (firstHalf && secondHalf) {
+    if (firstHalf.val !== secondHalf.val) {
+      return false;
+    }
+    firstHalf = firstHalf.next;
+    secondHalf = secondHalf.next;
+  }
+
+  // Restore the original linked list
+  slow = reversedHead;
+  reversedHead = null;
+  while (slow) {
+    const next = slow.next;
+    slow.next = reversedHead;
+    reversedHead = slow;
+    slow = next;
+  }
+
+  // Return the result
+  return true;
+}
