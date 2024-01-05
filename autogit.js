eac97cd0b66799bf2b57ddedeabc1c83e244aefd
RyannKim327 <weryses19@gmail.com>
2024-01-05 23:24:29 +0800

    1-5-2024 23:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,80 +1,25 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
-class Queue {
-  constructor() {
-    this.head = null; // points to the front of the queue
-    this.tail = null; // points to the end of the queue
-  }
-}
-enqueue(data) {
-  const newNode = new Node(data);
-  if (this.head === null) {
-    this.head = newNode;
-    this.tail = newNode;
-  } else {
-    this.tail.next = newNode;
-    this.tail = newNode;
-  }
-}
-dequeue() {
-  if (this.head === null) {
-    return null; // queue is empty
-  }
-  const data = this.head.data;
+// Insertion sort algorithm implementation in JavaScript
 
-  if (this.head === this.tail) {
-    this.head = null;
-    this.tail = null;
-  } else {
-    this.head = this.head.next;
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let currentElement = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && currentElement < array[j]) {
+      array[j + 1] = array[j];
+      j--;
+    }
+
+    array[j + 1] = currentElement;
   }
 
-  return data;
+  return array;
 }
-peek() {
-  if (this.head === null) {
-    return null; // queue is empty
-  }
-  return this.head.data;
-}
-isEmpty() {
-  return this.head === null;
-}
-size() {
-  let count = 0;
-  let current = this.head;
-  while (current) {
-    count++;
-    current = current.next;
-  }
-  return count;
-}
-const queue = new Queue();
 
-// Enqueue some items
-queue.enqueue(10);
-queue.enqueue(20);
-queue.enqueue(30);
+// Example usage:
 
-// Dequeue an item
-const dequeuedItem = queue.dequeue(); // dequeuedItem = 10
+let unsortedArray = [5, 2, 8, 3, 1, 9, 4, 7, 6];
+console.log("Unsorted array:", unsortedArray);
 
-// Get the value of the front item without removing it
-const peekedItem = queue.peek(); // peekedItem = 20
-
-// Check if the queue is empty
-const isEmpty = queue.isEmpty(); // isEmpty = false
-
-// Get the number of items in the queue
-const size = queue.size(); // size = 2
-
-// Print the items in the queue
-let current = queue.head;
-while (current) {
-  console.log(current.data); // 20, 30
-  current = current.next;
-}
+let sortedArray = insertionSort(unsortedArray);
+console.log("Sorted array:", sortedArray);
