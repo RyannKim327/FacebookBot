7150b3194093f96d664030d46cf4e817a622eef1
RyannKim327 <weryses19@gmail.com>
2024-01-05 19:18:55 +0800

    1-5-2024 19:18 [User mode]

--- autogit.js
+++
@@ -1,4 +0,0 @@
-const numbers = [5, 2, 9, 1, 3, 7, 4, 6, 8];
-numbers.sort((a, b) => a - b);
-console.log(numbers); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
-const sortedNumbers = numbers.slice().sort((a, b) => a - b);
