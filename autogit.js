f4ecda2f9f66b0a310cf1a55a5d61e3408a97a44
RyannKim327 <weryses19@gmail.com>
2024-01-05 23:06:36 +0800

    1-5-2024 23:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,23 @@
-// Function to find the median of two sorted arrays
-function findMedianSortedArrays(arr1, arr2) {
-  // Create a merged array of the two sorted arrays
-  let mergedArr = [];
-  let i = 0, j = 0;
-  while (i < arr1.length && j < arr2.length) {
-    if (arr1[i] < arr2[j]) {
-      mergedArr.push(arr1[i]);
-      i++;
-    } else {
-      mergedArr.push(arr2[j]);
-      j++;
-    }
-  }
-  while (i < arr1.length) {
-    mergedArr.push(arr1[i]);
-    i++;
-  }
-  while (j < arr2.length) {
-    mergedArr.push(arr2[j]);
-    j++;
-  }
+// Import the cron module.
+const cron = require('node-cron');
 
-  // Find the median of the merged array
-  let median;
-  const halfLength = Math.floor(mergedArr.length / 2);
-  if (mergedArr.length % 2 === 0) {
-    median = (mergedArr[halfLength] + mergedArr[halfLength - 1]) / 2;
-  } else {
-    median = mergedArr[halfLength];
-  }
+// Define a cron job that runs every minute.
+const job = cron.schedule('* * * * *', () => {
+  // Do something every minute.
+  console.log('Running a cron job every minute.');
+});
 
-  return median;
-}
-
-// Example usage
-const arr1 = [1, 3, 5, 7, 9];
-const arr2 = [2, 4, 6, 8, 10];
-console.log(findMedianSortedArrays(arr1, arr2)); // Output: 5.5
+// Start the cron job.
+job.start();
+cron.schedule('0 0 * * *', () => {
+  // Do something every day at midnight.
+});
+cron.schedule('0 8 * * 1', () => {
+  // Do something every Monday at 8am.
+});
+cron.schedule('*/15 * * * *', () => {
+  // Do something every 15 minutes.
+});
+cron.schedule('0 8 * * 1', () => {
+  // Do something every Monday at 8am.
+});
