5045d465d6a803368ea74c7fd6a02efcb75c1537
RyannKim327 <weryses19@gmail.com>
2024-01-05 19:10:36 +0800

    1-5-2024 19:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,3 @@
-function maxSubArraySum(arr) {
-  let maxSoFar = -Infinity;
-  let maxEndingHere = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    maxEndingHere = maxEndingHere + arr[i];
-    if (maxSoFar < maxEndingHere) {
-      maxSoFar = maxEndingHere;
-    }
-    if (maxEndingHere < 0) {
-      maxEndingHere = 0;
-    }
-  }
-  return maxSoFar;
-}
-const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-const maxSum = maxSubArraySum(arr);
-
-console.log("Maximum sum subarray:", maxSum);
-Maximum sum subarray: 6
+const str = "Hello world";
+const newStr = str.replace(/\s/g, "");
+console.log(newStr); // Output: "Helloworld"
