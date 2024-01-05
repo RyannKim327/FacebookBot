0d52fd17308695909b539eb340dd97748689694f
RyannKim327 <weryses19@gmail.com>
2024-01-06 00:56:58 +0800

    1-6-2024 0:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,122 +1,19 @@
-// Node class to represent a vertex in the graph
-class Node {
-  constructor(id, distance) {
-    this.id = id;
-    this.distance = distance;
-    this.visited = false;
+function findKthSmallest(array, k) {
+  // Check if the input is valid
+  if (!array || array.length < k || k <= 0) {
+    return null;
   }
+
+  // Sort the array in ascending order
+  array.sort((a, b) => a - b);
+
+  // Return the kth smallest element
+  return array[k - 1];
 }
+const array = [5, 3, 8, 2, 1, 4];
+const k = 3;
 
-// Graph class to represent the complete graph
-class Graph {
-  constructor() {
-    this.nodes = [];
-  }
+const kthSmallest = findKthSmallest(array, k);
 
-  // Add a node to the graph
-  addNode(id) {
-    const node = new Node(id, Infinity);
-    this.nodes.push(node);
-  }
-
-  // Add an edge between two nodes with a given weight
-  addEdge(source, destination, weight) {
-    const sourceNode = this.getNode(source);
-    const destinationNode = this.getNode(destination);
-
-    // Add an edge from source to destination
-    sourceNode.edges.push({ node: destinationNode, weight: weight });
-
-    // Add an edge from destination to source (undirected graph)
-    destinationNode.edges.push({ node: sourceNode, weight: weight });
-  }
-
-  // Get a node by its id
-  getNode(id) {
-    return this.nodes.find((node) => node.id === id);
-  }
-
-  // Dijkstra's algorithm for finding shortest paths
-  dijkstra(startNode) {
-    // Initialize distances from start node to all other nodes to infinity
-    this.nodes.forEach((node) => (node.distance = Infinity));
-
-    // Set the distance of the start node to itself to 0
-    startNode.distance = 0;
-
-    // Initialize a priority queue to store nodes based on their distances
-    const queue = [];
-    queue.push(startNode);
-
-    // While the queue is not empty
-    while (queue.length > 0) {
-      // Get the node with the smallest distance from the queue
-      const currentNode = queue.shift();
-
-      // If the current node has been visited, skip it
-      if (currentNode.visited) {
-        continue;
-      }
-
-      // Mark the current node as visited
-      currentNode.visited = true;
-
-      // Update distances of adjacent nodes
-      currentNode.edges.forEach((edge) => {
-        const adjacentNode = edge.node;
-        const newDistance = currentNode.distance + edge.weight;
-
-        // If the new distance is shorter than the current distance of the adjacent node, update the distance
-        if (newDistance < adjacentNode.distance) {
-          adjacentNode.distance = newDistance;
-          queue.push(adjacentNode);
-        }
-      });
-    }
-  }
-
-  // Get the shortest path from a start node to a destination node
-  getShortestPath(startNode, destinationNode) {
-    this.dijkstra(startNode);
-    const path = [];
-
-    // If the destination node is unreachable, return an empty array
-    if (destinationNode.distance === Infinity) {
-      return path;
-    }
-
-    // Backtrack from the destination node to the start node
-    let currentNode = destinationNode;
-    while (currentNode !== startNode) {
-      path.unshift(currentNode.id);
-      currentNode = this.getNode(
-        currentNode.edges.find((edge) => edge.node.id === currentNode.id).node.id
-      );
-    }
-
-    // Add the start node to the path
-    path.unshift(startNode.id);
-
-    return path;
-  }
-}
-
-// Example usage
-const graph = new Graph();
-graph.addNode('A');
-graph.addNode('B');
-graph.addNode('C');
-graph.addNode('D');
-graph.addNode('E');
-
-graph.addEdge('A', 'B', 5);
-graph.addEdge('A', 'C', 2);
-graph.addEdge('B', 'C', 1);
-graph.addEdge('C', 'D', 3);
-graph.addEdge('D', 'E', 1);
-
-const startNode = graph.getNode('A');
-const destinationNode = graph.getNode('E');
-
-const shortestPath = graph.getShortestPath(startNode, destinationNode);
-console.log(`Shortest path from ${startNode.id} to ${destinationNode.id}: ${shortestPath.join(' -> ')}`);
+console.log(`The ${k}th smallest element in the array is: ${kthSmallest}`);
+The 3th smallest element in the array is: 3
