f0b8468e3758d448c580235aa348678d71d8b1f9
RyannKim327 <weryses19@gmail.com>
2024-01-05 19:19:44 +0800

    1-5-2024 19:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,38 @@
-function insertionSort(array) {
-  for (var i = 1; i < array.length; i++) {
-    var currentValue = array[i];
-    var j = i - 1;
-    while (j >= 0 && currentValue < array[j]) {
-      array[j + 1] = array[j];
-      j--;
-    }
-    array[j + 1] = currentValue;
+function largestPrimeFactor(number) {
+  // Check if the number is valid
+  if (number <= 1) {
+    return "Invalid number";
   }
-  return array;
+
+  // Check if the number is even
+  if (number % 2 === 0) {
+    // Divide the number by 2 until it is odd
+    while (number % 2 === 0) {
+      number /= 2;
+    }
+  }
+
+  // Initialize the largest prime factor
+  var largestPrimeFactor = 1;
+
+  // Iterate from 3 to the square root of the number
+  var divisor = 3;
+  while (number > 1) {
+    // Check if the divisor is a prime factor
+    if (number % divisor === 0) {
+      // Update the largest prime factor
+      largestPrimeFactor = divisor;
+
+      // Divide the number by the divisor
+      number /= divisor;
+    } else {
+      // Increase the divisor by 2
+      divisor += 2;
+    }
+  }
+
+  // Return the largest prime factor
+  return largestPrimeFactor;
 }
+var largestPrimeFactor = largestPrimeFactor(13195);
+console.log(largestPrimeFactor); // 29
