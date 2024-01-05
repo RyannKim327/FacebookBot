6ff1aaf3e077b77880e1b4a73265ff499ea5f910
RyannKim327 <weryses19@gmail.com>
2024-01-06 01:19:35 +0800

    1-6-2024 1:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,65 +1,82 @@
 class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
+  constructor(id, parent, cost, heuristic) {
+    this.id = id;
+    this.parent = parent;
+    this.cost = cost;
+    this.heuristic = heuristic;
   }
 }
+function aStarSearch(startNode, goalNode) {
+  // Create open and closed sets
+  let openSet = [startNode];
+  let closedSet = [];
 
-class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
-
-  enqueue(value) {
-    const newNode = new Node(value);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
+  // While there are nodes in the open set
+  while (openSet.length > 0) {
+    // Find the node with the lowest total cost (cost + heuristic)
+    let currentNode = openSet[0];
+    let currentIndex = 0;
+    for (let i = 1; i < openSet.length; i++) {
+      if (openSet[i].cost + openSet[i].heuristic < currentNode.cost + currentNode.heuristic) {
+        currentNode = openSet[i];
+        currentIndex = i;
+      }
     }
-    this.size++;
-  }
 
-  dequeue() {
-    if (!this.head) {
-      return null;
+    // Move the current node from open set to closed set
+    openSet.splice(currentIndex, 1);
+    closedSet.push(currentNode);
+
+    // If the goal node is reached, return the path
+    if (currentNode === goalNode) {
+      let path = [];
+      let node = currentNode;
+      while (node != null) {
+        path.push(node);
+        node = node.parent;
+      }
+      return path.reverse();
     }
-    const value = this.head.value;
-    this.head = this.head.next;
-    this.size--;
-    if (!this.head) {
-      this.tail = null;
+
+    // Generate the neighbors of the current node
+    let neighbors = generateNeighbors(currentNode);
+
+    // For each neighbor
+    for (let neighbor of neighbors) {
+      // If the neighbor is in the closed set, skip it
+      if (closedSet.some(node => node === neighbor)) {
+        continue;
+      }
+
+      // Calculate the cost to reach the neighbor
+      let newCost = currentNode.cost + getDistance(currentNode, neighbor);
+
+      // If the neighbor is not in the open set or the new cost is lower
+      if (!openSet.some(node => node === neighbor) || newCost < neighbor.cost) {
+        // Update the neighbor's cost and heuristic value
+        neighbor.cost = newCost;
+        neighbor.heuristic = getDistance(neighbor, goalNode);
+
+        // Set the neighbor's parent to the current node
+        neighbor.parent = currentNode;
+
+        // If the neighbor is not in the open set, add it
+        if (!openSet.some(node => node === neighbor)) {
+          openSet.push(neighbor);
+        }
+      }
     }
-    return value;
   }
 
-  isEmpty() {
-    return this.size === 0;
-  }
-
-  getFront() {
-    return this.head ? this.head.value : null;
-  }
-
-  getSize() {
-    return this.size;
-  }
+  // If no path exists, return null
+  return null;
 }
-const queue = new Queue();
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
+function generateNeighbors(node) {
+  // Implement neighbor generation logic depending on your problem
+  // Return an array of neighboring nodes
+}
 
-console.log(queue.getFront()); // Output: 1
-console.log(queue.getSize()); // Output: 3
-
-console.log(queue.dequeue()); // Output: 1
-
-console.log(queue.getFront()); // Output: 2
-console.log(queue.getSize()); // Output: 2
-
-console.log(queue.isEmpty()); // Output: false
+function getDistance(nodeA, nodeB) {
+  // Implement distance calculation logic depending on your problem
+  // Return the distance between the two nodes
+}
