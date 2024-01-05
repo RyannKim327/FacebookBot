c1446960c087ec80f0b80d7cc0a554b993ac1194
RyannKim327 <weryses19@gmail.com>
2024-01-05 19:35:46 +0800

    1-5-2024 19:35 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,115 +1,35 @@
-class Node {
-  constructor(order, keys = [], children = []) {
-    this.order = order;
-    this.keys = keys;
-    this.children = children;
-  }
-}
-class BTree {
-  constructor(order) {
-    this.order = order;
-    this.root = new Node(order);
-  }
-}
-insert(key) {
-  this._insertKey(key, this.root);
-}
+function countingSort(arr) {
+  // Find the maximum value in the array
+  let max = Math.max(...arr);
 
-_insertKey(key, node) {
-  if (node.keys.length < this.order - 1) {
-    this._insertKeyIntoNode(key, node);
-  } else {
-    this._splitNode(key, node);
-  }
-}
-_splitNode(key, node) {
-  const medianKeyIndex = Math.floor(this.order / 2) - 1;
-  
-  const newRightNodeKeys = node.keys.slice(medianKeyIndex + 1);
-  const newRightNodeChildren = node.children.slice(medianKeyIndex + 1);
-  
-  node.keys = node.keys.slice(0, medianKeyIndex + 1);
-  node.children = node.children.slice(0, medianKeyIndex + 2);
-  
-  const newRightNode = new Node(this.order, newRightNodeKeys, newRightNodeChildren);
-  
-  const parentNode = node.parent;
-  if (parentNode) {
-    this._insertKeyIntoNode(key, parentNode);
-  } else {
-    this.root = new Node(this.order, [key], [node, newRightNode]);
-  }
-}
-search(key) {
-  return this._searchKey(key, this.root);
-}
+  // Create an array of zeros with a length equal to the maximum value + 1
+  let counts = new Array(max + 1).fill(0);
 
-_searchKey(key, node) {
-  let index = node.keys.indexOf(key);
-  if (index !== -1) {
-    return node;
+  // Store the count of each element in the counts array
+  for (let i = 0; i < arr.length; i++) {
+    counts[arr[i]]++;
   }
-  for (let i = 0; i < node.keys.length; i++) {
-    if (key < node.keys[i]) {
-      return this._searchKey(key, node.children[i]);
-    }
-  }
-  return this._searchKey(key, node.children[node.children.length - 1]);
-}
-delete(key) {
-  this._deleteKey(key, this.root);
-}
 
-_deleteKey(key, node) {
-  let index = node.keys.indexOf(key);
-  if (index !== -1) {
-    this._deleteKeyFromNode(key, node);
-  } else {
-    for (let i = 0; i < node.keys.length; i++) {
-      if (key < node.keys[i]) {
-        this._deleteKey(key, node.children[i]);
-        break;
-      }
-    }
-    if (i === node.keys.length) {
-      this._deleteKey(key, node.children[node.children.length - 1]);
-    }
+  // Update the counts array to contain the cumulative count of each element
+  for (let i = 1; i <= max; i++) {
+    counts[i] += counts[i - 1];
   }
-}
-_mergeNodes(leftNode, rightNode) {
-  leftNode.keys.push(...rightNode.keys);
-  leftNode.children.push(...rightNode.children);
-  
-  const parentNode = rightNode.parent;
-  const index = parentNode.children.indexOf(rightNode);
-  parentNode.children.splice(index, 1);
-  
-  if (parentNode.keys.length < this.order - 1) {
-    this._mergeKeys(parentNode, leftNode);
-  } else {
-    this._balanceNode(parentNode);
+
+  // Create an array to store the sorted output
+  let sortedArray = new Array(arr.length);
+
+  // Place each element in its correct position in the sorted array
+  for (let i = arr.length - 1; i >= 0; i--) {
+    let element = arr[i];
+    sortedArray[counts[element] - 1] = element;
+    counts[element]--;
   }
-}
-_balanceNode(node) {
-  const leftSibling = node.getLeftSibling();
-  const rightSibling = node.getRightSibling();
-  
-  if (leftSibling && leftSibling.keys.length > this.order - 1) {
-    this._redistributeKeys(leftSibling, node);
-  } else if (rightSibling && rightSibling.keys.length > this.order - 1) {
-    this._redistributeKeys(node, rightSibling);
-  } else {
-    this._mergeNodes(leftSibling, node);
+
+  // Copy the sorted array back to the input array
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = sortedArray[i];
   }
-}
-_redistributeKeys(leftNode, rightNode) {
-  const medianKeyIndex = Math.floor(this.order / 2) - 1;
-  
-  leftNode.keys.push(rightNode.keys[0]);
-  rightNode.keys = rightNode.keys.slice(1);
-  
-  if (leftNode.children.length < this.order) {
-    leftNode.children.push(rightNode.children[0]);
-    rightNode.children = rightNode.children.slice(1);
-  }
+
+  // Return the sorted array
+  return arr;
 }
