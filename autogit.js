595de0102879e48f58a2a49602ce72c0f8602e67
RyannKim327 <weryses19@gmail.com>
2024-01-05 23:20:56 +0800

    1-5-2024 23:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,20 @@
-// Function to find the longest common substring of two strings
-function longestCommonSubstring(str1, str2) {
-  // Create a matrix to store the lengths of the longest common substrings
-  // of the prefixes of str1 and str2
-  let matrix = new Array(str1.length + 1).fill(0).map(() => new Array(str2.length + 1).fill(0));
+// Function to check if a string is a palindrome
+function isPalindrome(str) {
+  // Convert the string to lowercase and remove all spaces
+  str = str.toLowerCase().replace(/\s/g, "");
 
-  // Find the longest common substring
-  let longestSubstring = "";
-  let longestSubstringLength = 0;
-  for (let i = 1; i <= str1.length; i++) {
-    for (let j = 1; j <= str2.length; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        matrix[i][j] = matrix[i - 1][j - 1] + 1;
-        if (matrix[i][j] > longestSubstringLength) {
-          longestSubstringLength = matrix[i][j];
-          longestSubstring = str1.substring(i - longestSubstringLength, i);
-        }
-      }
-    }
-  }
-
-  // Return the longest common substring
-  return longestSubstring;
+  // Check if the string is the same backwards and forwards
+  return str === str.split("").reverse().join("");
 }
+
+let str = "racecar";
+console.log(isPalindrome(str)); // true
+
+str = "madam";
+console.log(isPalindrome(str)); // true
+
+str = "hello";
+console.log(isPalindrome(str)); // false
+
+str = "A man, a plan, a canal, Panama!";
+console.log(isPalindrome(str)); // true (ignores spaces and punctuation)
