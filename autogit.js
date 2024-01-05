5414a6db99a22a540f4de86ab7ec628c7de21314
RyannKim327 <weryses19@gmail.com>
2024-01-06 01:31:12 +0800

    1-6-2024 1:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,55 +1,69 @@
-function merge(left, right) {
-  let result = [];
-  let i = 0, j = 0;
-
-  while (i < left.length && j < right.length) {
-    if (left[i] <= right[j]) {
-      result.push(left[i]);
-      i++;
-    } else {
-      result.push(right[j]);
-      j++;
-    }
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-
-  // Add any remaining elements from left or right subarrays
-  while (i < left.length) {
-    result.push(left[i]);
-    i++;
-  }
-
-  while (j < right.length) {
-    result.push(right[j]);
-    j++;
-  }
-
-  return result;
 }
-function mergeSort(arr) {
-  let arrayLength = arr.length;
-  let tempArray = arr.map((item) => [item]);
 
-  while (arrayLength > 1) {
-    let currentIndex = 0;
-
-    for (let i = 0; i < arrayLength; i += 2) {
-      let mergedArray = merge(tempArray[i], tempArray[i + 1]);
-      tempArray[currentIndex] = mergedArray;
-      currentIndex++;
-    }
-
-    if (arrayLength % 2 !== 0) {
-      tempArray[currentIndex] = tempArray[arrayLength - 1];
-      currentIndex++;
-    }
-
-    arrayLength = currentIndex;
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
   }
 
-  return tempArray[0];
+  insert(value) {
+    const newNode = new TreeNode(value);
+
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+
+  search(value) {
+    let currentNode = this.root;
+
+    while (currentNode) {
+      if (value === currentNode.value) {
+        return true;
+      } else if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+
+    return false;
+  }
+
+  // Additional operations like delete, traversal, etc. can be implemented here
 }
 
 // Usage example:
-let unsortedArray = [8, 5, 2, 9, 5, 6, 3];
-let sortedArray = mergeSort(unsortedArray);
-console.log(sortedArray);
+
+const bst = new BinarySearchTree();
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(2);
+bst.insert(7);
+
+console.log(bst.search(2)); // true
+console.log(bst.search(8)); // false
