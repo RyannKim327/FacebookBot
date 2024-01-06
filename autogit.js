9ffebd819c8c4edc17419acf2741a9885298e686
RyannKim327 <weryses19@gmail.com>
2024-01-06 22:06:20 +0800

    1-6-2024 22:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,5 @@
-const arr = [5, 2, 9, 1, 7];
+let str = "   Hello,    World!   ";
+let trimmedStr = str.replace(/\s/g, "");
 
-const max = Math.max(...arr);
-
-console.log(max); // Output: 9
-const arr = [5, 2, 9, 1, 7];
-
-const max = Math.max.apply(null, arr);
-
-console.log(max); // Output: 9
+console.log(trimmedStr);
+Hello,World!
