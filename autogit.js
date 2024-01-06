10342c01bef15ba104537a51cb2d03548e398904
RyannKim327 <weryses19@gmail.com>
2024-01-06 22:04:17 +0800

    1-6-2024 22:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,17 @@
-function buildTable(pattern) {
-  let table = [0];
-  let prefix = 0;
-  
-  for (let i = 1; i < pattern.length; i++) {
-    if (pattern[i] === pattern[prefix]) {
-      prefix++;
-    } else {
-      prefix = 0;
-    }
-    table[i] = prefix; 
-  }
-  
-  return table;
-}
-function knuthMorrisPratt(text, pattern) {
-  let table = buildTable(pattern);
-  let positions = [];
-  
-  let textIndex = 0;
-  let patternIndex = 0;
-  
-  while (textIndex < text.length) {
-    if (pattern[patternIndex] === text[textIndex]) {
-       if (patternIndex === pattern.length - 1) {
-         positions.push(textIndex - pattern.length + 1);
-         patternIndex = table[patternIndex];
-       }
-       patternIndex++;
-       textIndex++;
-    } else if (patternIndex > 0) {
-      patternIndex = table[patternIndex - 1];
-    } else {
-      patternIndex = 0;
-      textIndex++;
-    }
-  }
-  
-  return positions;
-}
-let text = "ABC ABCDAB ABCDABCDABDE";
-let pattern = "ABCDABD";
+function reverseWords(str) {
+  // Split the string into an array of words
+  var words = str.split(' ');
 
-let positions = knuthMorrisPratt(text, pattern);
-console.log("Pattern found at positions:", positions);
-Pattern found at positions: [15]
+  // Reverse the order of the words in the array
+  words.reverse();
+
+  // Join the words back into a string
+  var reversedStr = words.join(' ');
+
+  return reversedStr;
+}
+
+// Example usage
+var originalStr = "Hello, world!";
+var reversedStr = reverseWords(originalStr);
+console.log(reversedStr); // Output: "world! Hello,"
