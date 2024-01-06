b2825bec751ecba0806104dbfe75da73047dac7b
RyannKim327 <weryses19@gmail.com>
2024-01-06 22:05:28 +0800

    1-6-2024 22:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,4 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.children = [];
-  }
+let str = "Hello World!";
+let lowercaseStr = str.toLowerCase();
 
-  addChild(node) {
-    this.children.push(node);
-  }
-}
-
-function depthFirstSearch(node, target) {
-  if (node == null) {
-    return null;
-  }
-
-  if (node.value === target) {
-    return node;
-  }
-
-  for (let child of node.children) {
-    const result = depthFirstSearch(child, target);
-    if (result !== null) {
-      return result;
-    }
-  }
-
-  return null;
-}
-
-// Usage example:
-const rootNode = new Node(1);
-const node2 = new Node(2);
-const node3 = new Node(3);
-const node4 = new Node(4);
-const node5 = new Node(5);
-
-rootNode.addChild(node2);
-rootNode.addChild(node3);
-node2.addChild(node4);
-node3.addChild(node5);
-
-console.log(depthFirstSearch(rootNode, 5)); // Output: Node { value: 5, children: [] }
+console.log(lowercaseStr); // Output: hello world!
