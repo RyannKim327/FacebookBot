3055eb39117728192d041428be9675a19363e194
RyannKim327 <weryses19@gmail.com>
2024-01-06 22:01:17 +0800

    1-6-2024 22:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,54 @@
-Area = (base * height) / 2
-function calculateTriangleArea(base, height) {
-  var area = (base * height) / 2;
-  return area;
-}
-var triangleBase = 10;
-var triangleHeight = 5;
+function buildPrefixTable(pattern) {
+  const prefixTable = new Array(pattern.length);
+  let length = 0;
+  let i = 1;
+  prefixTable[0] = 0;
 
-var triangleArea = calculateTriangleArea(triangleBase, triangleHeight);
-console.log("Triangle area:", triangleArea);
+  while (i < pattern.length) {
+    if (pattern[i] === pattern[length]) {
+      length++;
+      prefixTable[i] = length;
+      i++;
+    } else {
+      if (length !== 0) {
+        length = prefixTable[length - 1];
+      } else {
+        prefixTable[i] = 0;
+        i++;
+      }
+    }
+  }
+
+  return prefixTable;
+}
+function kmpSearch(text, pattern) {
+  const prefixTable = buildPrefixTable(pattern);
+  const indices = [];
+  let i = 0; // index for text
+  let j = 0; // index for pattern
+
+  while (i < text.length) {
+    if (pattern[j] === text[i]) {
+      j++;
+      i++;
+    }
+
+    if (j === pattern.length) {
+      indices.push(i - j);
+      j = prefixTable[j - 1];
+    } else if (i < text.length && pattern[j] !== text[i]) {
+      if (j !== 0) {
+        j = prefixTable[j - 1];
+      } else {
+        i++;
+      }
+    }
+  }
+
+  return indices;
+}
+const text = "ABABDABACDABABCABAB";
+const pattern = "ABABCABAB";
+const indices = kmpSearch(text, pattern);
+
+console.log(`The pattern '${pattern}' is found at indices: ${indices}`);
