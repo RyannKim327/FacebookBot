293d7ac4393d509bc92f3bbba38e3eb036955996
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 16:15:01 +0800

    01-10-24 16:14

--- auto/openai.js
+++ auto/openai.js
@@ -4,7 +4,7 @@ module.exports = async (api, event) => {
 	let { data } = await axios.get(`https://hercai.onrender.com/turbo-16k/hercai?question=${encodeURI(event.body)}`)
 	api.setMessageReaction("🤔", event.messageID, (e) => {}, true)
 	api.sendTypingIndicator(event.threadID, (e) => {
-		api.sendMessage(`‎ ${data.response}`, event.threadID, (e, m) => {
+		api.sendMessage(`‎ ${data.reply}`, event.threadID, (e, m) => {
 			api.setMessageReaction("", event.messageID, (e) => {}, true)
 		})
 	})
