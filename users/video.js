e437e9cf89fecd7241e30b829d2de3e0a0442b5e
RyannKim327 <rksesgundo123@gmail.com>
2024-05-30 07:37:25 +0800

    05-30-24 07:37

--- users/video.js
+++ users/video.js
@@ -20,91 +20,97 @@ module.exports = async (api, event, regex) => {
 			}
 		}, event.messageID)
 	}
-	try{
-		const json = JSON.parse(fs.readFileSync("data/preferences.json"))
-		const file = fs.createWriteStream(`temp/${event.threadID}_${event.senderID}.mp4`)
-		api.setMessageReactionMqtt("🔎", event.messageID, (e) => {}, true)
-		const data = event.body.match(regex)[1]
-		const yt_1 = /youtube.com\/watch\?v=([a-zA-Z0-9\-_]{11}$)/
-		const yt_2 = /youtu.be\/([a-zA-Z0-9\-_]+)/
-		let music = {}
-		if(yt_1.test(data)){
-			music = {
-				"content": [
-					{
-						"videoId": data.match(yt_1)[1]
-					}
-				]
-			}
-		}else if(yt_2.test(data)){
-			music = {
-				"content": [
-					{
-						"videoId": data.match(yt_2)[1].split("?")[0]
-					}
-				]
-			}
-		}else{
-			await yt.initalize()
-			music = await yt.search(data.replace(/[^\w\s]/gi, ''), "video")
-			if(music.content.length <= 0){
-				throw new Error(`${data.replace(/[^\w\s]/gi, '')} returned no results found`)
+	api.sendMessage("Searching...", event.threadID, async (e, m) => {
+		const msgID = m.messageID
+		try{
+			const json = JSON.parse(fs.readFileSync("data/preferences.json"))
+			const file = fs.createWriteStream(`temp/${event.threadID}_${event.senderID}.mp4`)
+			api.setMessageReactionMqtt("🔎", event.messageID, (e) => {}, true)
+			const data = event.body.match(regex)[1]
+			const yt_1 = /youtube.com\/watch\?v=([a-zA-Z0-9\-_]+)/
+			const yt_2 = /youtu.be\/([a-zA-Z0-9\-_]+)/
+			let music = {}
+			if(yt_1.test(data)){
+				music = {
+					"content": [
+						{
+							"videoId": data.match(yt_1)[1]
+						}
+					]
+				}
+			}else if(yt_2.test(data)){
+				music = {
+					"content": [
+						{
+							"videoId": data.match(yt_2)[1].split("?")[0]
+						}
+					]
+				}
 			}else{
-				if(music.content[0].videoId == undefined){
-					throw new Error(`${data.replace(/[^\w\s]/gi, '')} is not found on youtube music. Try to add the singer, maybe I can find it.`)
+				await yt.initalize()
+				music = await yt.search(data.replace(/[^\w\s]/gi, ''), "video")
+				if(music.content.length <= 0){
+					throw new Error(`${data.replace(/[^\w\s]/gi, '')} returned no results found`)
+				}else{
+					if(music.content[0].videoId == undefined){
+						throw new Error(`${data.replace(/[^\w\s]/gi, '')} is not found on youtube music. Try to add the singer, maybe I can find it.`)
+					}
 				}
 			}
-		}
-		const url = `https://www.youtube.com/watch?v=${music.content[0].videoId}`
-		const strm = ytdl(url, {
-			quality: "lowest"
-		})
-		const info = await ytdl.getInfo(url)
-		api.setMessageReactionMqtt("⏳", event.messageID, (e) => {}, true)
-		let user = await api.getUserInfo(event.senderID)
-		let g = gender(user[event.senderID]['firstName'])['eng']
-		let reqBy = `${g} ${user[event.senderID]['name']}`
-		ffmpegs(strm).audioBitrate(96).save(`${__dirname}/../temp/${event.threadID}_${event.senderID}.mp4`).on("end", async () => {
-			api.sendMessage({
-				body: `Here's your requests ${reqBy}:\nTitle: ${font(info.videoDetails.title)}\nUploaded by: ${info.videoDetails.author.name}`,
-				mentions:[{
-					id: event.senderID,
-					tag: user[event.senderID]['name']
-				}],
-				attachment: fs.createReadStream(`${__dirname}/../temp/${event.threadID}_${event.senderID}.mp4`).on("end", async () => {
-					if(fs.existsSync(`${__dirname}/../temp/${event.threadID}_${event.senderID}.mp4`)){
-						setTimeout(() => {
-							fs.unlink(`${__dirname}/../temp/${event.threadID}_${event.senderID}.mp4`, (err) => {
-								if(err){
-									console.log(err)
-								}
-								api.setMessageReactionMqtt("", event.messageID, (e) => {}, true)
-								console.log("Done")
-							})
-						}, 500)
+			const url = `https://www.youtube.com/watch?v=${music.content[0].videoId}`
+			const strm = ytdl(url, {
+				quality: "lowest"
+			})
+			const info = await ytdl.getInfo(url)
+			api.setMessageReactionMqtt("⏳", event.messageID, (e) => {}, true)
+			api.editMessage("Processing...", msgID, (e, m) => {})
+			let user = await api.getUserInfo(event.senderID)
+			let g = gender(user[event.senderID]['firstName'])['eng']
+			let reqBy = `${g} ${user[event.senderID]['name']}`
+			ffmpegs(strm).audioBitrate(96).save(`${__dirname}/../temp/${event.threadID}_${event.senderID}.mp4`).on("end", async () => {
+				api.sendMessage({
+					body: `Here's your requests ${reqBy}:\nTitle: ${font(info.videoDetails.title)}\nUploaded by: ${info.videoDetails.author.name}`,
+					mentions:[{
+						id: event.senderID,
+						tag: user[event.senderID]['name']
+					}],
+					attachment: fs.createReadStream(`${__dirname}/../temp/${event.threadID}_${event.senderID}.mp4`).on("end", async () => {
+						if(fs.existsSync(`${__dirname}/../temp/${event.threadID}_${event.senderID}.mp4`)){
+							setTimeout(() => {
+								fs.unlink(`${__dirname}/../temp/${event.threadID}_${event.senderID}.mp4`, (err) => {
+									if(err){
+										console.log(err)
+										api.editMessage("Error", msgID, (e, m) => {})
+									}
+									api.setMessageReactionMqtt("", event.messageID, (e) => {}, true)
+									console.log("Done")
+									api.editMessage("File Uploaded", (e, m) => {})
+								})
+							}, 500)
+						}
+					})
+				}, event.threadID, (e, m) => {
+					if(e){
+						api.sendMessage(e.message, event.threadID, (e, m) => {
+							afk(api, json)
+						})
 					}
 				})
-			}, event.threadID, (e, m) => {
-				if(e){
-					api.sendMessage(e.message, event.threadID, (e, m) => {
-						afk(api, json)
-					})
-				}
-			})
-			api.setMessageReactionMqtt("", event.messageID, (e) => {}, true)
-		})
-	}catch(err){
-		console.log(err)
-		api.sendMessage("Error: " + err, event.threadID, event.messageID)
-		api.setMessageReactionMqtt("", event.messageID, (e) => {}, true)
-		if(fs.existsSync(`${__dirname}/../temp/${event.threadID}_${event.senderID}.mp4`)){
-			fs.unlink(`${__dirname}/../temp/${event.threadID}_${event.senderID}.mp4`, (err) => {
-				if(err){
-					console.log(err)
-				}
 				api.setMessageReactionMqtt("", event.messageID, (e) => {}, true)
-				console.log("Done")
 			})
+		}catch(err){
+			console.log(err)
+			api.sendMessage("Error: " + err, event.threadID, event.messageID)
+			api.setMessageReactionMqtt("", event.messageID, (e) => {}, true)
+			if(fs.existsSync(`${__dirname}/../temp/${event.threadID}_${event.senderID}.mp4`)){
+				fs.unlink(`${__dirname}/../temp/${event.threadID}_${event.senderID}.mp4`, (err) => {
+					if(err){
+						console.log(err)
+					}
+					api.setMessageReactionMqtt("", event.messageID, (e) => {}, true)
+					console.log("Done")
+				})
+			}
 		}
-	}
+	})
 }
